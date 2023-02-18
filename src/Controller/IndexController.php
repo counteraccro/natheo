36ffbfe5a0216d6e841639f57cc34f06e2b1884b
Aymeric <counteraccro@gmail.com>
2023-02-18 22:41:40 +0100

    Mise en place
    Mise en place du projet Natheo
    Installation de Bootstrap, Bootstrap-icon, webpack

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+    #[Route('/', name: 'app_index')]
+    public function index(): Response
+    {
+        return $this->render('index/index.html.twig', [
+            'controller_name' => 'IndexController',
+        ]);
+    }
+}
