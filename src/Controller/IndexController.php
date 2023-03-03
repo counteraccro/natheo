d9bceb3216f7fc6dfa1a2aa7e06f5abf564f09c2
Aymeric <counteraccro@gmail.com>
2023-03-03 19:06:12 +0100

    Test existance BDD

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -6,15 +6,29 @@
  */
 namespace App\Controller;
 
+use Doctrine\DBAL\Connection;
+use Doctrine\DBAL\Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/{_locale}', name: 'index_', requirements: ['_locale' => 'en|es|fr'])]
 class IndexController extends AbstractController
 {
+    /**
+     * @throws Exception
+     */
     #[Route('/', name: 'app_index')]
-    public function index(): Response
+    public function index(Connection $connection): Response
     {
+        $cs = $connection->createSchemaManager();
+        try {
+            var_dump($cs->listDatabases());
+        } catch (Exception $exception)
+        {
+            echo 'oki';
+        }
+
         return $this->render('index/index.html.twig', [
             'controller_name' => 'IndexController',
         ]);
