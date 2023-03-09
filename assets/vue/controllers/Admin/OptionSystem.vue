82cc0f63e05e8d731eaa9e519a403ab4edf4b58c
Aymeric <counteraccro@gmail.com>
2023-03-09 17:22:33 +0100

    Mise en place de vue.js pour les events onchange pour l'enregistrement des modifications

---
+++ assets/vue/controllers/Admin/OptionSystem.vue
@@ -0,0 +1,45 @@
+<script>
+
+/**
+ * @author Gourdon Aymeric
+ * @version 1.0
+ * Permet de générer les events pour la sauvegarde des options systèmes
+ */
+
+
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+export default {
+  name: "OptionSystem",
+  mounted() {
+
+    let change = this.OnChange;
+    let elements = document.getElementsByClassName('event-input');
+    for(let i = 0; i < elements.length; i++) {
+      (function(index) {
+        elements[index].addEventListener("change", change)
+      })(i);
+    }
+
+  },
+  methods: {
+    OnChange(event) {
+
+      let id = event.target.getAttribute('id');
+      alert(id);
+
+      //this.id = event.target.getAttribute('data-id');
+      //this.dataName = event.target.getAttribute('data-name');
+    }
+  }
+}
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+
+</style>
