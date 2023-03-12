a874c847b140403ce828b97807166957abdc6bbc
Aymeric <counteraccro@gmail.com>
2023-03-12 18:07:58 +0100

    ébauche tableau grid en vue.js

---
+++ assets/vue/controllers/Admin/SidebarGrid.vue
@@ -0,0 +1,34 @@
+<template>
+  <form id="search">
+    Search <input name="query" v-model="searchQuery">
+  </form>
+  <Grid
+      :data="gridData"
+      :columns="gridColumns"
+      :filter-key="searchQuery">
+  </Grid>
+</template>
+
+<script>
+import Grid from '../../Components/Grid.vue'
+export default {
+  name: "SidebarGrid",
+  components: {
+    Grid
+  },
+  data: () => ({
+    searchQuery: '',
+    gridColumns: ['name', 'power'],
+    gridData: [
+      { name: 'Chuck Norris', power: Infinity },
+      { name: 'Bruce Lee', power: 9000 },
+      { name: 'Jackie Chan', power: 7000 },
+      { name: 'Jet Li', power: 8000 }
+    ]
+  })
+}
+</script>
+
+<style scoped>
+
+</style>
