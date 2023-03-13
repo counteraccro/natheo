49c4a99c476156cba95de98f3d5b6ce561916061
Aymeric <counteraccro@gmail.com>
2023-03-13 16:40:50 +0100

    Ajout action pour récupérer la liste des sidebarElement

--- src/Controller/Admin/SidebarController.php
+++ src/Controller/Admin/SidebarController.php
@@ -6,7 +6,11 @@
  */
 namespace App\Controller\Admin;
 
+use App\Service\Admin\SidebarElementService;
+use App\Utils\Debug;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
@@ -28,6 +32,22 @@ class SidebarController extends AbstractController
 
         return $this->render('admin/sidebar/index.html.twig', [
             'breadcrumb' => $breadcrumb,
+            'page' => 1,
+            'limit' => 2
         ]);
+    }
+
+    /**
+     * Charge le tableau grid de sidebar en ajax
+     * @param Request $request
+     * @param SidebarElementService $sidebarElementService
+     * @return JsonResponse
+     */
+    #[Route('/ajax/load-grid-data', name: 'load_grid_data')]
+    public function loadGridData(Request $request, SidebarElementService $sidebarElementService): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $grid = $sidebarElementService->getAllFormatToGrid($data['page'], $data['limit']);
+        return $this->json($grid);
     }
 }
