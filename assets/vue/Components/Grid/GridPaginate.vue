dbba6eac6adf62999550bbe3b17605271c7d1397
Aymeric <counteraccro@gmail.com>
2023-03-15 10:29:51 +0100

    Mise en place de la pagination

--- assets/vue/Components/Grid/GridPaginate.vue
+++ assets/vue/Components/Grid/GridPaginate.vue
@@ -11,27 +11,54 @@ export default {
   },
   emits: ['change-page-event'],
   data() {
-    return {}
+    return {
+      cPage : this.currentPage
+    }
   },
   methods: {
     getNbPage() {
       return round(this.nbElementsTotal / this.nbElements);
     },
+
+    updateCurrentPage(page)
+    {
+      this.cPage = page;
+    }
   }
 }
 
 </script>
 
 <template>
-  <div>
-    <div v-for="(n, i) in this.getNbPage()">
-      <button class="btn btn-primary"  @click="$emit('change-page-event', n)">Page {{ n }}</button>
-    </div>
-    Nombre de page : {{ this.nbPage }}
-    Nombre de Element total : {{ this.nbElementsTotal }}
-    Nombre de Element : {{ this.nbElements }}
-  </div>
-
+  <nav>
+    <div class="float-end">Page <strong>{{cPage}}</strong> sur <strong>{{this.getNbPage()}}</strong> - {{this.nbElementsTotal}} elements</div>
+    <ul class="pagination pagination-sm">
+      <li class="page-item" :class="cPage === 1 ? 'disabled' : ''">
+        <button class="page-link" @click="[$emit('change-page-event', 1), updateCurrentPage(1)]">
+          <i class="bi bi-chevron-double-left"></i>
+        </button>
+      </li>
+      <li class="page-item" :class="cPage === 1 ? 'disabled' : ''">
+        <button class="page-link" @click="[$emit('change-page-event', (cPage-1)), updateCurrentPage((cPage-1))]">
+          <i class="bi bi-chevron-compact-left"></i>
+        </button>
+      </li>
+      <li class="page-item" v-for="(n, i) in this.getNbPage()" :class="cPage === n ? 'active' : ''">
+        <button v-if="n === cPage-1 || n === cPage+1 || n === cPage || n <= 2 || n >= this.getNbPage()-1" class="page-link" @click="[$emit('change-page-event', n), updateCurrentPage(n)]">{{ n }}</button>
+        <button v-else-if="n === cPage-2 || n === cPage+2" class="page-link disabled">...</button>
+      </li>
+      <li class="page-item" :class="cPage === this.getNbPage() ? 'disabled' : ''">
+        <button class="page-link" @click="[$emit('change-page-event', (cPage+1)), updateCurrentPage((cPage+1))]">
+          <i class="bi bi-chevron-compact-right"></i>
+        </button>
+      </li>
+      <li class="page-item" :class="cPage === this.getNbPage() ? 'disabled' : ''">
+        <button class="page-link" @click="[$emit('change-page-event', this.getNbPage()), updateCurrentPage(this.getNbPage())]">
+          <i class="bi bi-chevron-double-right"></i>
+        </button>
+      </li>
+    </ul>
+  </nav>
 </template>
 
 <style scoped>
