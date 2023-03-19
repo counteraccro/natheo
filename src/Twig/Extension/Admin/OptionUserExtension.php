02ba9a6111c34eaba28132a08a09c58e4ad6cb2c
Aymeric <counteraccro@gmail.com>
2023-03-19 18:42:04 +0100

    Ajout nouvelle fonction pour récupérer les options du user courant

---
+++ src/Twig/Extension/Admin/OptionUserExtension.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Twig\Extension\Admin;
+
+use App\Twig\Runtime\Admin\OptionUserExtensionRuntime;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+class OptionUserExtension extends AbstractExtension
+{
+    public function getFilters(): array
+    {
+        return [
+            // If your filter generates SAFE HTML, you should add a third
+            // parameter: ['is_safe' => ['html']]
+            // Reference: https://twig.symfony.com/doc/3.x/advanced.html#automatic-escaping
+            new TwigFilter('filter_name', [OptionUserExtensionRuntime::class, 'doSomething']),
+        ];
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('get_option_user_value_by_key', [OptionUserExtensionRuntime::class, 'getOptionValueByKey']),
+        ];
+    }
+}
---
+++ src/Twig/Runtime/Admin/OptionUserExtensionRuntime.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * @author Gourdon Aymeric
+ * @version 1.0
+ * Permet de générer le formulaire de saisie des options users
+ * Permet de récupérer la valeur d'une option
+ */
+namespace App\Twig\Runtime\Admin;
+
+use App\Entity\Admin\OptionUser;
+use App\Service\Admin\OptionUserService;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use Twig\Extension\RuntimeExtensionInterface;
+
+class OptionUserExtensionRuntime extends AppAdminExtensionRuntime implements RuntimeExtensionInterface
+{
+    /**
+     * @var OptionUserService
+     */
+    private OptionUserService $optionUserService;
+
+    public function __construct(RouterInterface $router, TranslatorInterface $translator, OptionUserService $optionUserService)
+    {
+        $this->optionUserService = $optionUserService;
+        parent::__construct($router, $translator);
+    }
+
+    /**
+     * Retourne la valeur de l'option en fonction de sa clé
+     * @param string $key
+     * @return string
+     */
+    public function getOptionValueByKey(string $key): string
+    {
+        /* @var OptionUser $optionUser */
+        $optionUser = $this->optionUserService->getByKey($key);
+        return $optionUser->getValue();
+    }
+}
