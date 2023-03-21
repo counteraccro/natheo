14e9291f1f70b7c0aaf224f2227759f341ef026f
Aymeric <counteraccro@gmail.com>
2023-03-21 13:44:36 +0100

    changement config webpack

--- webpack.config.js
+++ webpack.config.js
@@ -48,7 +48,7 @@ Encore
     .enableBuildNotifications()
     .enableSourceMaps(!Encore.isProduction())
     // enables hashed filenames (e.g. app.abc123.css)
-    .enableVersioning(Encore.isProduction())
+    //.enableVersioning(Encore.isProduction())
 
     // configure Babel
     // .configureBabel((config) => {
