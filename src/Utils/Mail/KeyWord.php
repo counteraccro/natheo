d7cba23667c09772c13d315ccf642ac21a3e22be
Aymeric <counteraccro@gmail.com>
2023-04-25 10:29:30 +0200

    Amélioration code

--- src/Utils/Mail/KeyWord.php
+++ src/Utils/Mail/KeyWord.php
@@ -4,6 +4,7 @@
  * @author Gourdon Aymeric
  * @version 1.0
  */
+
 namespace App\Utils\Mail;
 
 use App\Entity\Admin\User;
@@ -38,6 +39,12 @@ class KeyWord
     private const USER_LASTNAME = 'user.lastname';
 
     /**
+     * Clé du login de l'admin qui réalise l'action
+     * @var string
+     */
+    private const ADMIN_LOGIN_ACTION = 'admin.login';
+
+    /**
      * Clé nom du site
      * @var string
      */
@@ -56,18 +63,29 @@ class KeyWord
     private const GLOBAL_URL_CHANGE_PASSWORD = 'global.url_change_psw';
 
     /**
+     * Clé pour les mots clés globaux
+     * @var string
+     */
+    private const KEY_GLOBAL = 'global';
+
+    /**
      * Tableau contenant l'ensemble des keyWords de tous les emails
      * @var array|array[]
      */
     private array $tabKeyWord = [
-        MailService::KEY_MAIL_CHANGE_PASSWORD => [
+        self::KEY_GLOBAL => [
             self::USER_EMAIL => '',
             self::USER_LOGIN => '',
             self::USER_FIRSTNAME => '',
             self::USER_LASTNAME => '',
             self::GLOBAL_URL => '',
-            self::GLOBAL_URL_CHANGE_PASSWORD => '',
             self::GLOBAL_SITE_NAME => ''
+        ],
+        MailKey::KEY_MAIL_CHANGE_PASSWORD => [
+            self::GLOBAL_URL_CHANGE_PASSWORD => '',
+        ],
+        MailKey::KEY_MAIL_ACCOUNT_ADM_DISABLE => [
+            self::ADMIN_LOGIN_ACTION => '',
         ]
     ];
 
@@ -82,7 +100,8 @@ class KeyWord
      */
     public function __construct(string $key)
     {
-        $this->currentTabKeyWord = $this->tabKeyWord[$key];
+        $this->currentTabKeyWord = $this->tabKeyWord[self::KEY_GLOBAL];
+        $this->currentTabKeyWord = array_merge($this->currentTabKeyWord, $this->tabKeyWord[$key]);
     }
 
     /**
@@ -96,31 +115,82 @@ class KeyWord
     }
 
     /**
-     * Renvoi le tableau de keyWord avec les valeurs correspondantes pour l'email
-     * change password
+     * Renvoi sous la forme d'un tableau les keyWords + leurs valeurs associés
      * @param User $user
-     * @param UrlGeneratorInterface $router
+     * @param OptionSystemService $optionSystemService
      * @return array
      */
-    public function getTabKeyWordMailChangePassword(User                $user, UrlGeneratorInterface $router,
-                                                    OptionSystemService $optionSystemService): array
+    private function getGlobalKeyWord(User $user, OptionSystemService $optionSystemService): array
     {
         $url = $optionSystemService->getValueByKey(OptionSystemService::OS_ADRESSE_SITE);
         $siteName = $optionSystemService->getValueByKey(OptionSystemService::OS_SITE_NAME);
 
-        $tab = [
+        return [
             '[[' . self::USER_LOGIN . ']]' => $user->getLogin(),
             '[[' . self::USER_EMAIL . ']]' => $user->getEmail(),
             '[[' . self::USER_FIRSTNAME . ']]' => $user->getFirstname(),
             '[[' . self::USER_LASTNAME . ']]' => $user->getLastname(),
-            '[[' . self::GLOBAL_URL . ']]' => $url . $router->generate('index_index'),
-            '[[' . self::GLOBAL_URL_CHANGE_PASSWORD . ']]' => $url . $router->generate('index_index'),
+            '[[' . self::GLOBAL_URL . ']]' => $url,
             '[[' . self::GLOBAL_SITE_NAME . ']]' => $siteName,
         ];
+    }
 
+    /**
+     * Format le retour sous la forme d'un tableau avec 2 clés search et replace
+     * @param array $return
+     * @return array
+     */
+    private function formatReturnValue(array $return): array
+    {
         return [
-            'search' => array_keys($tab),
-            'replace' => array_values($tab)
+            'search' => array_keys($return),
+            'replace' => array_values($return)
         ];
+    }
+
+    /**
+     * Renvoi le tableau de keyWord avec les valeurs correspondantes pour l'email
+     * change password
+     * @param User $user
+     * @param UrlGeneratorInterface $router
+     * @param OptionSystemService $optionSystemService
+     * @return array
+     */
+    public function getMailChangePassword(User                $user, UrlGeneratorInterface $router,
+                                                    OptionSystemService $optionSystemService
+    ): array
+    {
+        $url = $optionSystemService->getValueByKey(OptionSystemService::OS_ADRESSE_SITE);
+
+        $tab = $this->getGlobalKeyWord($user, $optionSystemService);
+        $tab2 = [
+            '[[' . self::GLOBAL_URL_CHANGE_PASSWORD . ']]' => $url . $router->generate('index_index'),
+        ];
+        $tab = array_merge($tab, $tab2);
+
+        return $this->formatReturnValue($tab);
+    }
+
+    /**
+     * Renvoi le tableau de keyWord avec les valeurs correspondantes pour l'email
+     * disabled account admin
+     * @param User $user
+     * @param User $admin
+     * @param UrlGeneratorInterface $router
+     * @param OptionSystemService $optionSystemService
+     * @return array
+     */
+    public function getTabMailAccountAdmDisabled(User                  $user, User $admin,
+                                                 UrlGeneratorInterface $router,
+                                                 OptionSystemService   $optionSystemService
+    ): array
+    {
+        $tab = $this->getGlobalKeyWord($user, $optionSystemService);
+        $tab2 = [
+            '[[' . self::ADMIN_LOGIN_ACTION . ']]' => $admin->getLogin(),
+        ];
+        $tab = array_merge($tab, $tab2);
+
+        return $this->formatReturnValue($tab);
     }
 }
