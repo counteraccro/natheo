7632a0257ea39c2e033ff117923a70fad4991f66
Aymeric <counteraccro@gmail.com>
2023-04-25 11:23:03 +0200

    Ajout template générique

--- src/Utils/Mail/MailTemplate.php
+++ src/Utils/Mail/MailTemplate.php
@@ -8,13 +8,18 @@ namespace App\Utils\Mail;
 
 class MailTemplate
 {
+
+    private const EMAIL_PATH = 'emails' . DIRECTORY_SEPARATOR;
+
     /**
      * Path du dossier user pour les emails
      */
-    private const EMAIL_USER_PATH = 'emails' . DIRECTORY_SEPARATOR . 'user' . DIRECTORY_SEPARATOR;
+    private const EMAIL_USER_PATH = self::EMAIL_PATH . 'user' . DIRECTORY_SEPARATOR;
 
     /**
      * Template email nouveau mot de passe
      */
     public const EMAIL_USER_NEW_PASSWORD = self::EMAIL_USER_PATH . 'new_password.html.twig';
+
+    public const EMAIL_SIMPLE_TEMPLATE = self::EMAIL_PATH . 'simple_template.html.twig';
 }
