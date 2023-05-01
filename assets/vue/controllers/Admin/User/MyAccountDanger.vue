d03f72214d78a8e3775d204846e2ca6e21d2f3d0
Aymeric <counteraccro@gmail.com>
2023-05-01 16:32:15 +0200

    Création vue pour la danger zone

---
+++ assets/vue/controllers/Admin/User/MyAccountDanger.vue
@@ -0,0 +1,25 @@
+<template>
+
+</template>
+
+<script>
+export default {
+    name: "MyAccountDanger",
+    props: {
+
+    },
+    data() {
+        return {
+
+        }
+    },
+
+    mounted() {
+
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
