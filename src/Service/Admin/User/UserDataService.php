06b20123aaf59c9db5ad94ec5a74013078cf0c7a
Aymeric <counteraccro@gmail.com>
2023-06-04 17:37:13 +0200

    Mise en place méthode pour récupération des userData

---
+++ src/Service/Admin/User/UserDataService.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Service\Admin\User;
+
+use App\Entity\Admin\User;
+use App\Entity\Admin\UserData;
+use App\Service\Admin\AppAdminService;
+use App\Utils\User\UserdataKey;
+use DateTime;
+
+class UserDataService extends AppAdminService
+{
+
+    /**
+     * Met à jour une dataUser avec value en fonction de sa clé et du user
+     * @param string $key
+     * @param mixed $value
+     * @param User $user
+     * @return void
+     */
+    public function update(string $key, mixed $value, User $user): void
+    {
+        $userData = $this->findKeyAndUser($key, $user);
+        if ($userData === null) {
+            $userData = new UserData();
+            $userData->setUser($user)->setKey($key)->setValue($value);
+        } else {
+            $userData->setValue($value);
+        }
+        $this->save($userData);
+    }
+
+    /**
+     * Recherche une dataUser en fonction de sa clé et du user
+     * @param string $key
+     * @param User $user
+     * @return UserData|null
+     */
+    public function findKeyAndUser(string $key, User $user): ?UserData
+    {
+        $repo = $this->getRepository(UserData::class);
+        return $repo->findOneBy(['key' => $key, 'user' => $user]);
+    }
+
+    /**
+     * Retourne la dernière date de connexion
+     *  (cle KEY_LAST_CONNEXION)
+     * @param User $user
+     * @return DateTime|null
+     */
+    public function getLastConnexion(User $user): ?DateTime
+    {
+        $userData = $user->getUserDataByKey(UserdataKey::KEY_LAST_CONNEXION);
+
+        if ($userData === null) {
+            return null;
+        }
+
+        $lastConnexion = new DateTime();
+        $lastConnexion->setTimestamp($userData->getValue());
+        return $lastConnexion;
+    }
+}
---
+++ src/Utils/User/UserdataKey.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @author Gourdon Aymeric
+ * @version 1.0
+ * Clé pour userdata
+ */
+
+namespace App\Utils\User;
+
+class UserdataKey
+{
+    /**
+     * Clé pour le reset de mot de passe
+     * @var string
+     */
+    const KEY_RESET_PASSWORD = 'KEY_RESET_PASSWORD';
+
+    /**
+     * Clé pour la date de dernière connexion
+     * @var string
+     */
+    const KEY_LAST_CONNEXION = 'KEY_LAST_CONNEXION';
+
+}
