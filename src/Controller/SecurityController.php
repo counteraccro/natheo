740a1a8dde97741090078195d394dac12d19105e
Aymeric <counteraccro@gmail.com>
2023-06-07 08:15:16 +0200

    Mise en place formulaire de changement de mot de passe

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,12 +7,17 @@
 
 namespace App\Controller;
 
+use App\Entity\Admin\User;
 use App\Service\Admin\TranslateService;
 use App\Service\Admin\User\UserService;
 use App\Service\SecurityService;
+use Doctrine\ORM\NonUniqueResultException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
@@ -45,9 +50,13 @@ class SecurityController extends AbstractController
     /**
      * Permet de changer le mot de passe d'un User (Accès Admin)
      * @param string $key
+     * @param SecurityService $securityService
+     * @param TranslatorInterface $translator
+     * @param UserService $userService
      * @return Response
+     * @throws NonUniqueResultException
      */
-    #[Route(path: 'change-password/{key}', name: 'change_password', methods: ['GET'])]
+    #[Route(path: 'change-password/{key}', name: 'change_password_user', methods: ['GET'])]
     public function changePasswordAdm(
         string              $key,
         SecurityService     $securityService,
@@ -60,11 +69,35 @@ class SecurityController extends AbstractController
         if ($user === null) {
             throw $this->createNotFoundException($translator->trans('user.change_password.error_404', domain: 'user'));
         }
-
-        echo $user->getLogin();
-
         return $this->render('security/admin/change_password.html.twig', [
             'changePasswordTranslate' => $userService->getTranslateChangePassword(),
+            'user' => $user
+        ]);
+    }
+
+    /**
+     * Changement de mot passe du user
+     * @param User $user
+     * @param UserService $userService
+     * @param Request $request
+     * @param TranslatorInterface $translator
+     * @return JsonResponse
+     */
+    #[Route('/change-password/update/{id}', name: 'change_password_update_user', methods: ['POST'])]
+    public function updatePassword(
+        User $user,
+        UserService         $userService,
+        Request             $request,
+        TranslatorInterface $translator
+    ): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+        $userService->updatePassword($user, $data['data']);
+
+        return $this->json([
+            'status' => 'success',
+            'msg' => $translator->trans('user.change_password.success', domain: 'user'),
+            'redirect' => $this->generateUrl('auth_user_login')
         ]);
     }
 }
