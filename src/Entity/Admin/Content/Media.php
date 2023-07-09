9944c0adf03286c3920290b820f2c8360c4bbc91
Aymeric <counteraccro@gmail.com>
2023-07-09 11:29:25 +0200

    Création entité media

---
+++ src/Entity/Admin/Content/Media.php
@@ -0,0 +1,207 @@
+<?php
+
+namespace App\Entity\Admin\Content;
+
+use App\Entity\Admin\System\User;
+use App\Repository\Admin\Content\MediaRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+#[ORM\Entity(repositoryClass: MediaRepository::class)]
+#[ORM\Table(name: 'natheo.media')]
+class Media
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\ManyToOne(inversedBy: 'medias')]
+    private ?MediaFolder $mediaFolder = null;
+
+    #[ORM\ManyToOne(inversedBy: 'medias')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?User $user = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $type = null;
+
+    #[ORM\Column(length: 50)]
+    private ?string $extension = null;
+
+    #[ORM\Column]
+    private ?bool $disabled = false;
+
+    #[ORM\Column]
+    private ?bool $trash = false;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $path = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $web_path = null;
+
+    #[ORM\Column]
+    #[Gedmo\Timestampable(on: "create")]
+    private ?\DateTimeImmutable $created_at = null;
+
+    #[ORM\Column(nullable: true)]
+    #[Gedmo\Timestampable(on: "update")]
+    private ?\DateTimeImmutable $update_at = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getMediaFolder(): ?MediaFolder
+    {
+        return $this->mediaFolder;
+    }
+
+    public function setMediaFolder(?MediaFolder $mediaFolder): static
+    {
+        $this->mediaFolder = $mediaFolder;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): static
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): static
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): static
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getExtension(): ?string
+    {
+        return $this->extension;
+    }
+
+    public function setExtension(string $extension): static
+    {
+        $this->extension = $extension;
+
+        return $this;
+    }
+
+    public function isDisabled(): ?bool
+    {
+        return $this->disabled;
+    }
+
+    public function setDisabled(bool $disabled): static
+    {
+        $this->disabled = $disabled;
+
+        return $this;
+    }
+
+    public function isTrash(): ?bool
+    {
+        return $this->trash;
+    }
+
+    public function setTrash(bool $trash): static
+    {
+        $this->trash = $trash;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->created_at;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $created_at): static
+    {
+        $this->created_at = $created_at;
+
+        return $this;
+    }
+
+    public function getUpdateAt(): ?\DateTimeImmutable
+    {
+        return $this->update_at;
+    }
+
+    public function setUpdateAt(?\DateTimeImmutable $update_at): static
+    {
+        $this->update_at = $update_at;
+
+        return $this;
+    }
+
+    public function getPath(): ?string
+    {
+        return $this->path;
+    }
+
+    public function setPath(string $path): static
+    {
+        $this->path = $path;
+
+        return $this;
+    }
+
+    public function getWebPath(): ?string
+    {
+        return $this->web_path;
+    }
+
+    public function setWebPath(string $web_path): static
+    {
+        $this->web_path = $web_path;
+
+        return $this;
+    }
+}
---
+++ src/Repository/Admin/Content/MediaRepository.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Repository\Admin\Content;
+
+use App\Entity\Admin\Content\Media;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @extends ServiceEntityRepository<Media>
+ *
+ * @method Media|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Media|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Media[]    findAll()
+ * @method Media[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class MediaRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Media::class);
+    }
+
+    public function save(Media $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->persist($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+    public function remove(Media $entity, bool $flush = false): void
+    {
+        $this->getEntityManager()->remove($entity);
+
+        if ($flush) {
+            $this->getEntityManager()->flush();
+        }
+    }
+
+//    /**
+//     * @return Media[] Returns an array of Media objects
+//     */
+//    public function findByExampleField($value): array
+//    {
+//        return $this->createQueryBuilder('m')
+//            ->andWhere('m.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->orderBy('m.id', 'ASC')
+//            ->setMaxResults(10)
+//            ->getQuery()
+//            ->getResult()
+//        ;
+//    }
+
+//    public function findOneBySomeField($value): ?Media
+//    {
+//        return $this->createQueryBuilder('m')
+//            ->andWhere('m.exampleField = :val')
+//            ->setParameter('val', $value)
+//            ->getQuery()
+//            ->getOneOrNullResult()
+//        ;
+//    }
+}
