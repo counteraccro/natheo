8869f04fb327fc52bb3aef4c801e1779da04f833
Aymeric <counteraccro@gmail.com>
2023-07-12 11:27:33 +0200

    Class de constante pour les médiafolder

---
+++ src/Utils/Content/Media/MediaFolderConst.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Utils\Content\Media;
+
+class MediaFolderConst
+{
+    /**
+     * Nom du dossier root de la mediatheque
+     */
+    public const ROOT_FOLDER = 'mediatheque';
+}
