b98884d7005d3fbe05bd4d7c34a483fe437b9e48
Aymeric <counteraccro@gmail.com>
2023-08-12 14:26:51 +0200

    Changement $path de varchar en text

--- src/Entity/Admin/Content/MediaFolder.php
+++ src/Entity/Admin/Content/MediaFolder.php
@@ -27,7 +27,7 @@ class MediaFolder
     #[ORM\Column(length: 150)]
     private ?string $name = null;
 
-    #[ORM\Column(length: 255)]
+    #[ORM\Column(type: Types::TEXT)]
     private ?string $path = null;
 
     #[ORM\Column]
