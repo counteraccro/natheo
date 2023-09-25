2d862e96ad62a5791f6fba4cf8fd33c546b834b9
counteraccro <counteraccro@gmail.com>
2023-09-25 08:29:04 +0200

    Gestion des pages
    Mise en place de la gestion des pages

---
+++ src/Controller/Admin/Content/PageController.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @author Gourdon Aymeric
+ * @version 1.0
+ * Controller pour la gestion des pages
+ */
+namespace App\Controller\Admin\Content;
+
+use App\Controller\Admin\AppAdminController;
+use App\Utils\Breadcrumb;
+use App\Utils\System\Options\OptionUserKey;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[Route('/admin/{_locale}/page', name: 'admin_page_', requirements: ['_locale' => '%app.supported_locales%'])]
+#[IsGranted('ROLE_CONTRIBUTEUR')]
+class PageController extends AppAdminController
+{
+    #[Route('/', name: 'index')]
+    public function index(): Response
+    {
+
+        $breadcrumb = [
+            Breadcrumb::DOMAIN => 'page',
+            Breadcrumb::BREADCRUMB => [
+                'page.index.page_title_h1' => '#'
+            ]
+        ];
+
+        return $this->render('admin/content/page/index.html.twig', [
+            'breadcrumb' => $breadcrumb,
+            'page' => 1,
+            'limit' => $this->optionUserService->getValueByKey(OptionUserKey::OU_NB_ELEMENT)
+        ]);
+    }
+}
