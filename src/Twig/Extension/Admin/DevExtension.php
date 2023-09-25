117870ad65c8ce2a9a11449caf53a5bd3e55f3e5
counteraccro <counteraccro@gmail.com>
2023-09-25 11:40:50 +0200

    Mise en place information développement en mode débug

---
+++ src/Twig/Extension/Admin/DevExtension.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Twig\Extension\Admin;
+
+use App\Twig\Runtime\Admin\DevExtensionRuntime;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
+use Twig\TwigFunction;
+
+class DevExtension extends AbstractExtension
+{
+    public function getFilters(): array
+    {
+        return [
+            // If your filter generates SAFE HTML, you should add a third
+            // parameter: ['is_safe' => ['html']]
+            // Reference: https://twig.symfony.com/doc/3.x/advanced.html#automatic-escaping
+            new TwigFilter('getVersion', [DevExtensionRuntime::class, 'getVersion'], ['is_safe' => ['html']]),
+        ];
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('function_name', [DevExtensionRuntime::class, 'doSomething']),
+        ];
+    }
+}
---
+++ src/Twig/Runtime/Admin/DevExtensionRuntime.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * @author Gourdon Aymeric
+ * @version 1.0
+ * Retourne des infos pour le développement
+ */
+namespace App\Twig\Runtime\Admin;
+
+use App\Twig\Runtime\AppExtensionRuntime;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+use Twig\Extension\RuntimeExtensionInterface;
+
+class DevExtensionRuntime extends AppExtensionRuntime implements RuntimeExtensionInterface
+{
+    /**
+     * @var ParameterBagInterface
+     */
+    private ParameterBagInterface $parameterBag;
+
+
+    public function __construct(
+        ParameterBagInterface $parameterBag,
+        RouterInterface $router,
+        TranslatorInterface $translator)
+    {
+        $this->parameterBag = $parameterBag;
+        parent::__construct($router, $translator);
+    }
+
+    /**
+     * Retourne des informations de dev
+     * @return string
+     */
+    public function getVersion(): string
+    {
+        $version = $this->parameterBag->get('app.version');
+        $debug = $this->parameterBag->get('kernel.debug');
+
+        $return = '';
+
+        if($debug)
+        {
+            $return = $this->getDevInfo();
+        }
+        else {
+            $return = '<i class="bi bi-bug-fill"></i> <i>
+            ' . $this->translator->trans('dev.info.version', domain: 'dev') . ' <b>' . $version . '</b></i>';
+        }
+
+        return $return;
+    }
+
+    private function getDevInfo()
+    {
+        $version = $this->parameterBag->get('app.version');
+        return '<fieldset>
+        <legend class="text-white">' . $this->translator->trans('dev.info', domain: 'dev') . '</legend>
+            <i class="bi bi-github"></i> <i>
+            ' . $this->translator->trans('dev.info.branche', domain: 'dev') . ' <b>master</b></i> <br />
+            <i class="bi bi-bug-fill"></i> <i>
+            ' . $this->translator->trans('dev.info.version', domain: 'dev') . ' <b>' . $version . '</b></i> 
+        </fieldset>';
+    }
+}
