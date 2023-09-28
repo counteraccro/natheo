96f36ec2a9ee11f87807b0e27e53f0ad69b92d9a
counteraccro <counteraccro@gmail.com>
2023-09-28 10:12:25 +0200

    Gestion des pages
    Avancement sur les fixtures

--- src/DataFixtures/Admin/Content/Page/PageFixtures.php
+++ src/DataFixtures/Admin/Content/Page/PageFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures\Admin\Content\Page;
 
 use App\DataFixtures\AppFixtures;
+use App\Entity\Admin\Content\Page\Page;
 use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
 use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
@@ -16,10 +17,37 @@ class PageFixtures extends AppFixtures implements FixtureGroupInterface, Ordered
     {
         $data = Yaml::parseFile($this->pathDataFixtures . self::PAGE_FIXTURES_DATA_FILE);
 
-        // $product = new Product();
-        // $manager->persist($product);
+        foreach($data['pages'] as $ref => $data) {
+
+            $page = new Page();
+            foreach ($data as $key => $value) {
+                switch ($key) {
+                    case "user" :
+                        $page->setUser($this->getReference($value));
+                        break;
+                    default:
+                        $this->setData($key, $value, $page);
+                }
+            }
+
+            $manager->persist($page);
+            $this->addReference($ref, $page);
+        }
 
         $manager->flush();
+    }
+
+    /**
+     * Set une valeur pour l'objet Page
+     * @param string $key
+     * @param mixed $value
+     * @param mixed $entity
+     * @return void
+     */
+    private function setData(string $key, mixed $value, mixed $entity): void
+    {
+        $func = 'set' . ucfirst($key);
+        $entity->$func($value);
     }
 
     public static function getGroups(): array
--- src/Utils/Content/Page/PageConst.php
+++ src/Utils/Content/Page/PageConst.php
@@ -9,11 +9,37 @@ namespace App\Utils\Content\Page;
 
 class PageConst
 {
+    /**
+     * Affichage du rendu sur 1 colonne
+     * @var string
+     */
     const RENDER_1_COLUM = 1;
 
+    /**
+     * Affichage du rendu sur 2 colonnes
+     * @var string
+     */
     const RENDER_2_COLUM = 2;
 
+    /**
+     * Affichage du rendu sur 3 colonnes
+     * @var string
+     */
     const RENDER_3_COLUM = 3;
+
+    /**
+     * Status publié pour un article
+     * @var string
+     */
+    const STATUS_PUBLISH = 1;
+
+    /**
+     * Status brouillon pour un article
+     * @var string
+     */
+    const STATUS_DRAFT = 2;
+
+
 
 
 }
