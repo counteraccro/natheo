cd7c0d77d76178dddb1cc62a2fecaecb30665a0b
counteraccro <counteraccro@gmail.com>
2023-09-28 09:51:49 +0200

    Gestion des pages
    Avancement

---
+++ src/Utils/Content/Page/PageConst.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Constantes pour les pages
+ * @author Gourdon Aymeric
+ * @version 1.0
+ */
+
+namespace App\Utils\Content\Page;
+
+class PageConst
+{
+    const RENDER_1_COLUM = 1;
+
+    const RENDER_2_COLUM = 2;
+
+    const RENDER_3_COLUM = 3;
+
+
+}
