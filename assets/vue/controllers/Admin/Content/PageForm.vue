266c136e54c38b8f05918f3f86070ffc059acac1
Aymeric <counteraccro@gmail.com>
2023-10-03 10:07:04 +0200

    Création de la vue Js pour le formulaire de création / édition d'une page

---
+++ assets/vue/controllers/Admin/Content/PageForm.vue
@@ -0,0 +1,28 @@
+<script>
+/**
+ * Permet d'ajouter ou éditer une page
+ * @author Gourdon Aymeric
+ * @version 1.0
+ */
+
+export default {
+  props: {
+    url: String,
+    translate: Object,
+    locales: Object
+  },
+  emits: [],
+  data() {
+    return {}
+  },
+  computed: {},
+  methods: {
+  }
+}
+
+</script>
+
+<template>
+  <div>Template Page</div>
+
+</template>
