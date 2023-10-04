2fedfd6215fc1b6fc18663858ba1f7a0f11f8452
Aymeric <counteraccro@gmail.com>
2023-10-04 10:28:19 +0200

    Gestion des pages
    Ajout d'une méthode pour récupérer la liste de constantes de la classe

--- src/Utils/Content/Page/PageStatistiqueKey.php
+++ src/Utils/Content/Page/PageStatistiqueKey.php
@@ -11,4 +11,15 @@ class PageStatistiqueKey
     const KEY_PAGE_NB_VISITEUR = 'PAGE_NB_VISITEUR';
 
     const KEY_PAGE_NB_READ = 'PAGE_NB_READ';
+
+    /**
+     * Retourne la liste de constantes de la class
+     * @return array
+     */
+    public static function getConstants() : array
+    {
+        // "static::class" here does the magic
+        $reflectionClass = new \ReflectionClass(static::class);
+        return $reflectionClass->getConstants();
+    }
 }
