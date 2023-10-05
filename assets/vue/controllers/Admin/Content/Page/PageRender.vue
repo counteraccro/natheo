f654229bbd1cc8122fe3fd3922b37fe307629afc
Aymeric <counteraccro@gmail.com>
2023-10-05 23:01:59 +0200

    Avancement Gestion des pages
    Ebauche design

---
+++ assets/vue/controllers/Admin/Content/Page/PageRender.vue
@@ -0,0 +1,33 @@
+<script>/**
+ * Permet de générer un rendu pour les pages
+ * @author Gourdon Aymeric
+ * @version 1.0
+ */
+
+
+export default {
+  name: 'PageRender',
+  props: {
+    url: String,
+    translate: Object,
+  },
+  emits: [],
+  data() {
+    return {
+      page: [],
+    }
+  },
+  mounted() {
+
+  },
+  computed: {},
+  methods: {
+
+  }
+}
+</script>
+
+<template>
+  <div>render</div>
+
+</template>
