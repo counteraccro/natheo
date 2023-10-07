10ab773e59ee63ac2a4c19b8623ab595f7d79d02
Aymeric <counteraccro@gmail.com>
2023-10-07 09:54:45 +0200

    Suppression code inutile

--- assets/vue/controllers/Admin/Content/Page/PageForm.vue
+++ assets/vue/controllers/Admin/Content/Page/PageForm.vue
@@ -28,8 +28,7 @@ export default {
      * Permet de changer la locale pour la création d'une page
      * @param event
      */
-    switchLocale(event)
-    {
+    switchLocale(event) {
       this.currentLocale = event.target.value;
     }
   }
@@ -39,37 +38,27 @@ export default {
 </script>
 
 <template>
-  <div>Template Page</div>
-
-  <div v-for="pTrans in this.page.pageTranslations">
-    <input type="text" v-model="pTrans.locale">
+  <nav>
+    <select class="form-select float-end w-25" @change="this.switchLocale($event)">
+      <option value="" selected>{{ this.translate.select_locale }}</option>
+      <option v-for="(language, key) in this.locales.localesTranslate" :value="key" :selected="key===this.currentLocale">{{ language }}</option>
+    </select>
+    <div class="nav nav-pills mb-3" id="nav-tab-option-system" role="tablist">
+      <button class="nav-link active" id="nav-0-tab" data-bs-toggle="tab" data-bs-target="#nav-0" type="button" role="tab" aria-selected="true">
+        {{ this.translate.onglet_content }}
+      </button>
+      <button class="nav-link" id="nav-1-tab" data-bs-toggle="tab" data-bs-target="#nav-1" type="button" role="tab" aria-selected="false" tabindex="-1">
+        {{ this.translate.onglet_seo }}
+      </button>
+      <button class="nav-link " id="nav-2-tab" data-bs-toggle="tab" data-bs-target="#nav-2" type="button" role="tab" aria-selected="false" tabindex="-1">
+        {{ this.translate.onglet_tags }}
+      </button>
+    </div>
+  </nav>
+  <div class="tab-content" id="myTabContent">
+    <div class="tab-pane fade show active" id="nav-0" role="tabpanel" aria-labelledby="home-tab" tabindex="0">Tab0</div>
+    <div class="tab-pane fade" id="nav-1" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">Tab1</div>
+    <div class="tab-pane fade" id="nav-2" role="tabpanel" aria-labelledby="contact-tab" tabindex="0">Tab2</div>
   </div>
-
-  <div v-for="pTrans in this.page.pageTranslations">
-    <div>{{ pTrans.locale }}</div>
-  </div>
-
-      <nav>
-        <select class="form-select float-end w-25" @change="this.switchLocale($event)">
-          <option value="" selected>{{ this.translate.select_locale }} </option>
-          <option v-for="(language, key) in this.locales.localesTranslate" :value="key" :selected="key===this.currentLocale">{{ language }}</option>
-        </select>
-        <div class="nav nav-pills mb-3" id="nav-tab-option-system" role="tablist">
-          <button class="nav-link active" id="nav-0-tab" data-bs-toggle="tab" data-bs-target="#nav-0" type="button" role="tab" aria-selected="true">
-            {{ this.translate.onglet_content }}
-          </button>
-          <button class="nav-link" id="nav-1-tab" data-bs-toggle="tab" data-bs-target="#nav-1" type="button" role="tab" aria-selected="false" tabindex="-1">
-            {{ this.translate.onglet_seo }}
-          </button>
-          <button class="nav-link " id="nav-2-tab" data-bs-toggle="tab" data-bs-target="#nav-2" type="button" role="tab" aria-selected="false" tabindex="-1">
-            {{ this.translate.onglet_tags }}
-          </button>
-        </div>
-      </nav>
-      <div class="tab-content" id="myTabContent">
-        <div class="tab-pane fade show active" id="nav-0" role="tabpanel" aria-labelledby="home-tab" tabindex="0">Tab0</div>
-        <div class="tab-pane fade" id="nav-1" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">Tab1</div>
-        <div class="tab-pane fade" id="nav-2" role="tabpanel" aria-labelledby="contact-tab" tabindex="0">Tab2</div>
-      </div>
 
 </template>
