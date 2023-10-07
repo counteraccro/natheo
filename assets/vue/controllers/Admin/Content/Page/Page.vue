5f22076761584b63b37eb23ecc642d99e0d0eccb
Aymeric <counteraccro@gmail.com>
2023-10-07 10:22:22 +0200

    Gestion des pages
    Mise en place du chargement de l'onglet courant

--- assets/vue/controllers/Admin/Content/Page/Page.vue
+++ assets/vue/controllers/Admin/Content/Page/Page.vue
@@ -17,6 +17,7 @@ export default {
     return {
       page: this.pPage,
       currentLocale: '',
+      currentTab: 'content',
     }
   },
   mounted() {
@@ -25,11 +26,29 @@ export default {
   computed: {},
   methods: {
     /**
-     * Permet de changer la locale pour la création d'une page
+     * Permet de changer la locale pour la création/édition d'une page
      * @param event
      */
     switchLocale(event) {
       this.currentLocale = event.target.value;
+    },
+
+    /**
+     * Permet de changer d'onglet
+     * @param tab
+     */
+    switchTab(tab)
+    {
+      this.currentTab = tab;
+      this.loadTab();
+    },
+
+    /**
+     * Charge le contenu de l'onglet courant
+     */
+    loadTab()
+    {
+      console.log(this.currentTab);
     }
   }
 }
@@ -38,27 +57,28 @@ export default {
 </script>
 
 <template>
+
   <nav>
     <select class="form-select float-end w-25" @change="this.switchLocale($event)">
       <option value="" selected>{{ this.translate.select_locale }}</option>
       <option v-for="(language, key) in this.locales.localesTranslate" :value="key" :selected="key===this.currentLocale">{{ language }}</option>
     </select>
     <div class="nav nav-pills mb-3" id="nav-tab-option-system" role="tablist">
-      <button class="nav-link active" id="nav-0-tab" data-bs-toggle="tab" data-bs-target="#nav-0" type="button" role="tab" aria-selected="true">
+      <button class="nav-link active" @click="this.switchTab('content')" id="content-tab" data-bs-toggle="tab" data-bs-target="#nav-content" type="button" role="tab" aria-selected="true">
         {{ this.translate.onglet_content }}
       </button>
-      <button class="nav-link" id="nav-1-tab" data-bs-toggle="tab" data-bs-target="#nav-1" type="button" role="tab" aria-selected="false" tabindex="-1">
+      <button class="nav-link" @click="this.switchTab('seo')" id="seo-tab" data-bs-toggle="tab" data-bs-target="#nav-seo" type="button" role="tab" aria-selected="false" tabindex="-1">
         {{ this.translate.onglet_seo }}
       </button>
-      <button class="nav-link " id="nav-2-tab" data-bs-toggle="tab" data-bs-target="#nav-2" type="button" role="tab" aria-selected="false" tabindex="-1">
+      <button class="nav-link" @click="this.switchTab('tags')" id="tags-tab" data-bs-toggle="tab" data-bs-target="#nav-tags" type="button" role="tab" aria-selected="false" tabindex="-1">
         {{ this.translate.onglet_tags }}
       </button>
     </div>
   </nav>
   <div class="tab-content" id="myTabContent">
-    <div class="tab-pane fade show active" id="nav-0" role="tabpanel" aria-labelledby="home-tab" tabindex="0">Tab0</div>
-    <div class="tab-pane fade" id="nav-1" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">Tab1</div>
-    <div class="tab-pane fade" id="nav-2" role="tabpanel" aria-labelledby="contact-tab" tabindex="0">Tab2</div>
+    <div class="tab-pane fade show active" id="nav-content" role="tabpanel" aria-labelledby="home-tab" tabindex="0">Tab0</div>
+    <div class="tab-pane fade" id="nav-seo" role="tabpanel" aria-labelledby="profile-tab" tabindex="0">Tab1</div>
+    <div class="tab-pane fade" id="nav-tags" role="tabpanel" aria-labelledby="contact-tab" tabindex="0">Tab2</div>
   </div>
 
 </template>
