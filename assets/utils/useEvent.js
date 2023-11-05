c76479c93e79935642066fc5ed3cbdb53418c0ea
Aymeric <counteraccro@gmail.com>
2023-11-05 18:38:18 +0100

    Création d'un event globale

---
+++ assets/utils/useEvent.js
@@ -0,0 +1,5 @@
+import Emittery from 'emittery';
+const emitter = new Emittery();
+// Export the Emittery class and its instance.
+// The `emitter` instance is more important for us here
+export {emitter, Emittery};
