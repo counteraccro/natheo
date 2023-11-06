e49d751b86a7e13a0c0c95aee62081ead77c0e30
Aymeric <counteraccro@gmail.com>
2023-11-06 20:34:07 +0100

    Ajout génération automatique de l'url en fonction du titre

--- assets/vue/Components/Page/PageContentForm.vue
+++ assets/vue/Components/Page/PageContentForm.vue
@@ -15,17 +15,31 @@ export default {
   },
   emits: ['auto-save'],
   data() {
-    return {
-    }
+    return {}
   },
   mounted() {
 
   },
   computed: {},
   methods: {
-    autoSave()
-    {
+    /**
+     * Lance l'autoSave
+     */
+    autoSave() {
       this.$emit('auto-save', this.page);
+    },
+
+    /**
+     * Si l'url est vide, génère l'url en fonction du titre
+     */
+    generateUrl(title) {
+
+      this.page.pageTranslations.forEach((pageTranslation, index) => {
+        if (pageTranslation.locale === this.locale && pageTranslation.url === '') {
+          pageTranslation.url = title.replaceAll(' ', '-');
+        }
+      });
+      this.autoSave();
     }
   }
 }
@@ -33,6 +47,16 @@ export default {
 </script>
 
 <template>
+
+  <h5>aa</h5>
+
+  <div class="mb-3">
+    <label for="page-titre" class="form-label">{{ this.translate.input_titre_label }}</label>
+    <div v-for="pageTranslation in this.page.pageTranslations">
+      <input v-if="pageTranslation.locale === this.locale" type="text" class="form-control" id="page-titre" v-model="pageTranslation.titre" @change="this.generateUrl(pageTranslation.titre)">
+    </div>
+    <div id="pageTitreHelp" class="form-text">{{ this.translate.input_titre_info }}</div>
+  </div>
 
   <div class="mb-3">
     <label for="page-url" class="form-label">{{ this.translate.input_url_label }}</label>
@@ -42,12 +66,5 @@ export default {
     <div id="pageUrlHelp" class="form-text">{{ this.translate.input_url_info }}</div>
   </div>
 
-  <div class="mb-3">
-    <label for="page-titre" class="form-label">{{ this.translate.input_titre_label }}</label>
-    <div v-for="pageTranslation in this.page.pageTranslations">
-      <input v-if="pageTranslation.locale === this.locale" type="text" class="form-control" id="page-titre" v-model="pageTranslation.titre">
-    </div>
-    <div id="pageTitreHelp" class="form-text">{{ this.translate.input_titre_info }}</div>
-  </div>
 
 </template>
