d47863cd13cab2695a7957f87690ee146a6dcec2
Aymeric <counteraccro@gmail.com>
2024-01-05 13:24:57 +0100

    Mise en place vue js pour la création d'une FAQ

---
+++ assets/vue/controllers/Admin/Content/Faq/Faq.vue
@@ -0,0 +1,47 @@
+<script>
+/**
+ * @author Gourdon Aymeric
+ * @version 1.0
+ * Formulaire de création / édition d'une FAQ
+ */
+import axios from "axios";
+
+export default {
+  name: "Faq",
+  components: {},
+  props: {
+    urls: Object,
+    translate: Object,
+    locales: Object,
+    id: Number
+  },
+  data() {
+    return {
+    }
+  },
+  mounted() {
+    this.loadData();
+  },
+  methods: {
+
+    /**
+     * Chargement des données
+     */
+    loadData() {
+      /*axios.post(this.url_select, {
+        'time': this.time
+      }).then((response) => {
+        this.select = response.data.files;
+        this.trans = response.data.trans;
+      }).catch((error) => {
+        console.log(error);
+      }).finally();*/
+    },
+  }
+}
+</script>
+
+<template>
+  <div>Template FAQ</div>
+
+</template>
