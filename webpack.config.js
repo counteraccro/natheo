b3a56a914cbc8074f23a676d4848af0bb4a6e0e1
Aymeric <counteraccro@gmail.com>
2024-01-20 09:59:07 +0100

    Correction warning

--- webpack.config.js
+++ webpack.config.js
@@ -82,7 +82,8 @@ Encore
     .enableVueLoader()
     .addPlugin(new webpack.DefinePlugin({
         __VUE_OPTIONS_API__: true,
-        __VUE_PROD_DEVTOOLS__: false
+        __VUE_PROD_DEVTOOLS__: false,
+        __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: false
     }))
 ;
 
