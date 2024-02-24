864a6bc10a3d74ad6ae6c3c3ec40a4f7e6236c6b
Aymeric <counteraccro@gmail.com>
2024-02-24 22:15:28 +0100

    Mise en place des erreurs de controles

--- src/Utils/Global/OrderEntity.php
+++ src/Utils/Global/OrderEntity.php
@@ -8,6 +8,9 @@
 namespace App\Utils\Global;
 
 use Doctrine\Common\Collections\Collection;
+use Exception;
+use http\Client\Response;
+use Psr\Container\NotFoundExceptionInterface;
 
 class OrderEntity
 {
@@ -19,10 +22,16 @@ class OrderEntity
 
     private string $propertyName;
 
+    /**
+     * @param Collection $elements
+     * @param string $propertyName
+     * @throws Exception
+     */
     public function __construct(Collection $elements, string $propertyName = 'renderOrder')
     {
         $this->elements = $elements;
         $this->propertyName = $propertyName;
+        $this->verifPropertyExist();
     }
 
     /**
@@ -31,6 +40,7 @@ class OrderEntity
      * @param int $idOrder
      * @param string $action
      * @return void
+     * @throws Exception
      */
     public function orderByIdByAction(int $idNewOrder, int $idOrder, string $action = self::ACTION_BEFORE): void
     {
@@ -43,6 +53,8 @@ class OrderEntity
         if ($action === self::ACTION_AFTER) {
             $newOrder = $newOrder + 1;
         }
+
+        
 
         echo 'New order ' . $newOrder . '<br />';
 
@@ -67,10 +79,37 @@ class OrderEntity
         return $this->elements;
     }
 
-    private function getElementById(int $id)
+
+    /**
+     * Retourne un element de la liste par son id
+     * @param int $id
+     * @return object
+     * @throws Exception
+     */
+    private function getElementById(int $id): object
     {
-        return $this->elements->filter(function (object $element) use ($id) {
+        $element = $this->elements->filter(function (object $element) use ($id) {
             return $element->getId() === $id;
         })->first();
+
+        if (!$element) {
+            throw new Exception('Item #' . $id . ' not found in list');
+        } else {
+            return $element;
+        }
+    }
+
+    /**
+     * Vérifie si la propriété existe dans l'objet
+     * @return void
+     * @throws Exception
+     */
+    private function verifPropertyExist():void
+    {
+        $element = $this->elements->first();
+        if (!property_exists($element::class, $this->propertyName)) {
+            throw new Exception($this->propertyName .
+                '() property does not exist for object ' . $element::class);
+        }
     }
 }
