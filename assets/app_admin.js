651b8a51943a35dece502a2739b0b1ebc9883573
Aymeric <counteraccro@gmail.com>
2024-04-05 10:11:23 +0200

    Ajout de masonry-layout

--- assets/app_admin.js
+++ assets/app_admin.js
@@ -7,6 +7,7 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import 'bootstrap';
 import bsCustomFileInput from 'bs-custom-file-input';
+import 'masonry-layout';
 
 
 // start the Stimulus application
