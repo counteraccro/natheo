4d684fca46a840d6242f2ce2370b19110ea52077
Aymeric <counteraccro@gmail.com>
2024-04-12 23:32:31 +0200

    Ajout nouvelle class JS pour le copier coller

---
+++ assets/utils/copyToClipboard.js
@@ -0,0 +1,25 @@
+/**
+ * Permet de faire un copier coller
+ * @param textToCopy
+ * @returns {Promise<void>}
+ */
+
+export const copyToClipboard = async (textToCopy) => {
+    if (navigator.clipboard && window.isSecureContext) {
+        await navigator.clipboard.writeText(textToCopy);
+    } else {
+        const textArea = document.createElement("textarea");
+        textArea.value = textToCopy;
+        textArea.style.position = "absolute";
+        textArea.style.left = "-999999px";
+        document.body.prepend(textArea);
+        textArea.select();
+        try {
+            document.execCommand('copy');
+        } catch (error) {
+            console.error(error);
+        } finally {
+            textArea.remove();
+        }
+    }
+}
