202619c3d4a72e940ba973591d79ae02b2e50e27
Aymeric <counteraccro@gmail.com>
2024-04-19 10:15:10 +0200

    Mise en place du squellette du controller

--- src/Controller/Admin/Tools/SqlManagerController.php
+++ src/Controller/Admin/Tools/SqlManagerController.php
@@ -2,21 +2,37 @@
 /**
  * @author Gourdon Aymeric
  * @version 1.0
- * Controller pour la gestion des faqs
+ * Controller pour la gestion des requêtes SQL
  */
 namespace App\Controller\Admin\Tools;
 
+use App\Controller\Admin\AppAdminController;
+use App\Utils\Breadcrumb;
+use App\Utils\System\Options\OptionUserKey;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
-class SqlManagerController extends AbstractController
+#[Route('/admin/{_locale}/sql-manager', name: 'admin_sql_manager_',
+    requirements: ['_locale' => '%app.supported_locales%'])]
+#[IsGranted('ROLE_CONTRIBUTEUR')]
+class SqlManagerController extends AppAdminController
 {
-    #[Route('/admin/tools/sql/manager', name: 'app_admin_tools_sql_manager')]
+    #[Route('/', name: 'index')]
     public function index(): Response
     {
+        $breadcrumb = [
+            Breadcrumb::DOMAIN => 'sql_manager',
+            Breadcrumb::BREADCRUMB => [
+                'sql_manager.index.page_title_h1' => '#'
+            ]
+        ];
+
         return $this->render('admin/tools/sql_manager/index.html.twig', [
-            'controller_name' => 'SqlManagerController',
+            'breadcrumb' => $breadcrumb,
+            'page' => 1,
+            'limit' => $this->optionUserService->getValueByKey(OptionUserKey::OU_NB_ELEMENT)
         ]);
     }
 }
