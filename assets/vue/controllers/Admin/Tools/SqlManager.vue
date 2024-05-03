0005de4a59ef4523d90408de3eaff644091aec81
Aymeric <counteraccro@gmail.com>
2024-05-03 18:04:22 +0200

    Ebauche interface

--- assets/vue/controllers/Admin/Tools/SqlManager.vue
+++ assets/vue/controllers/Admin/Tools/SqlManager.vue
@@ -60,5 +60,48 @@ export default {
       </div>
     </div>
 
-  </div>
+    <div>
+      <label for="sql-textarea" class="form-label">Example textarea</label>
+      <textarea class="form-control" id="sql-textarea" rows="10"></textarea>
+    </div>
+
+    <div class="card mt-4">
+      <div class="card-header">
+        Featured
+        <div class="float-end btn btn-secondary btn-sm">aa</div>
+      </div>
+      <div class="card-body">
+        <div class="row">
+          <div class="col-6">
+            <label for="sql-table" class="form-label">Example textarea</label>
+            <select class="form-select" multiple id="sql-table">
+              <option selected>Open this select menu</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
+          </div>
+          <div class="col-6">
+            <label for="sql-table" class="form-label">Example textarea</label>
+            <select class="form-select" multiple id="sql-table">
+              <option selected>Open this select menu</option>
+              <option value="1">One</option>
+              <option value="2">Two</option>
+              <option value="3">Three</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </div>
+
+      <div class="card mt-4">
+        <div class="card-header">
+          Featured
+        </div>
+        <div class="card-body">
+
+        </div>
+      </div>
+
+    </div>
 </template>
