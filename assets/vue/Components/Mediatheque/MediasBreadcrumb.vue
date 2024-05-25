ead18b468ecd03769a3ec40c42495b8bc3a972ee
Aymeric <counteraccro@gmail.com>
2024-05-25 10:14:23 +0200

    Correction problème de lien

--- assets/vue/Components/Mediatheque/MediasBreadcrumb.vue
+++ assets/vue/Components/Mediatheque/MediasBreadcrumb.vue
@@ -33,10 +33,10 @@ export default {
       <li class="breadcrumb-item text-white" aria-current="page">
         <i class="bi bi-house-fill"></i>&nbsp;
         <span v-if="isEmpty(this.paths)">Home</span>
-        <a v-else href="#" class="text-white" @click="$emit('load-folder', 0)">Home</a>
+        <a v-else class="text-white" style="cursor: pointer" @click="$emit('load-folder', 0)">Home</a>
       </li>
       <li v-for="item in this.paths" class="breadcrumb-item text-white" :class="this.paths.indexOf(item) === Object.keys(this.paths).length-1 ? 'active' : ''">
-        <a v-if="this.paths.indexOf(item) !== Object.keys(this.paths).length-1" href="#" class="text-white" @click="$emit('load-folder', item.id)">{{ item.name }}</a>
+        <a v-if="this.paths.indexOf(item) !== Object.keys(this.paths).length-1" class="text-white" style="cursor: pointer" @click="$emit('load-folder', item.id)">{{ item.name }}</a>
         <span v-else>{{ item.name }}</span>
       </li>
     </ol>
