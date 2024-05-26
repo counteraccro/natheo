d104c8ec013d368c06ea918b005b42b5845fa62b
Aymeric <counteraccro@gmail.com>
2024-05-26 18:22:47 +0200

    rwk translate user

--- src/Service/Admin/System/User/UserService.php
+++ src/Service/Admin/System/User/UserService.php
@@ -245,58 +245,6 @@ class UserService extends AppAdminService
         }
 
         return $actions;
-
-    }
-
-    /**
-     * Renvoi les traductions pour le changement de mot de passe
-     * @return array
-     */
-    public function getTranslateChangePassword(): array
-    {
-        return [
-            'password' => $this->translator->trans('user.change_password.input.password_1', domain: 'user'),
-            'password_2' => $this->translator->trans('user.change_password.input.password_2', domain: 'user'),
-            'force' => $this->translator->trans('user.change_password.force', domain: 'user'),
-            'force_nb_character' => $this->translator->trans('user.change_password.force.nb_character', domain: 'user'),
-            'force_majuscule' => $this->translator->trans('user.change_password.force.majuscule', domain: 'user'),
-            'force_minuscule' => $this->translator->trans('user.change_password.force.minuscule', domain: 'user'),
-            'force_chiffre' => $this->translator->trans('user.change_password.force.chiffre', domain: 'user'),
-            'force_character_spe' => $this->translator->trans(
-                'user.change_password.force.character_spe',
-                domain: 'user'
-            ),
-            'error_password_2' => $this->translator->trans('user.error.password_2', domain: 'user'),
-        ];
-    }
-
-    /**
-     * Renvoi les traductions pour la danger-zone de mon profil
-     * @return array
-     */
-    public function getTranslateDangerZone(): array
-    {
-        return [
-            'loading' => $this->translator->trans('user.danger_zone.loading', domain: 'user'),
-            'btn_disabled' => $this->translator->trans('user.danger_zone.btn_disabled', domain: 'user'),
-            'btn_delete' => $this->translator->trans('user.danger_zone.btn_delete', domain: 'user'),
-            'btn_replace' => $this->translator->trans('user.danger_zone.btn_replace', domain: 'user'),
-            'disabled_title' => $this->translator->trans('user.danger_zone.title_disabled', domain: 'user'),
-            'disabled_content_1' => $this->translator->trans('user.danger_zone.content_disabled_1', domain: 'user'),
-            'disabled_content_2' => $this->translator->trans('user.danger_zone.content_disabled_2', domain: 'user'),
-            'disabled_btn_cancel' => $this->translator->trans('user.danger_zone.btn_cancel_disabled', domain: 'user'),
-            'disabled_btn_confirm' => $this->translator->trans('user.danger_zone.btn_confirm_disabled', domain: 'user'),
-            'delete_btn_cancel' => $this->translator->trans('user.danger_zone.btn_cancel_delete', domain: 'user'),
-            'delete_btn_confirm' => $this->translator->trans('user.danger_zone.btn_confirm_delete', domain: 'user'),
-            'delete_title' => $this->translator->trans('user.danger_zone.title_delete', domain: 'user'),
-            'delete_content_1' => $this->translator->trans('user.danger_zone.content_delete_1', domain: 'user'),
-            'delete_content_2' => $this->translator->trans('user.danger_zone.content_delete_2', domain: 'user'),
-            'replace_btn_cancel' => $this->translator->trans('user.danger_zone.btn_cancel_replace', domain: 'user'),
-            'replace_btn_confirm' => $this->translator->trans('user.danger_zone.btn_confirm_replace', domain: 'user'),
-            'replace_title' => $this->translator->trans('user.danger_zone.title_replace', domain: 'user'),
-            'replace_content_1' => $this->translator->trans('user.danger_zone.content_replace_1', domain: 'user'),
-            'replace_content_2' => $this->translator->trans('user.danger_zone.content_replace_2', domain: 'user'),
-        ];
     }
 
     /**
