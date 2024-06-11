c27b24a75511b0294fbe6562fc0b3ee24938b78d
Aymeric <counteraccro@gmail.com>
2024-06-11 15:21:57 +0200

    Test génération SQL

--- src/MessageHandler/Tools/DumpSqlHandler.php
+++ src/MessageHandler/Tools/DumpSqlHandler.php
@@ -12,6 +12,12 @@ use App\Repository\Admin\System\UserRepository;
 use App\Service\Admin\NotificationService;
 use App\Utils\Notification\NotificationKey;
 use App\Utils\Tools\DatabaseManager\DatabaseManagerConst;
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\DBAL\Schema\Table;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\SchemaTool;
+use Doctrine\Persistence\AbstractManagerRegistry;
 use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
@@ -22,6 +28,7 @@ class DumpSqlHandler
 
     public function __construct(
         private NotificationService $notificationService,
+        private EntityManagerInterface $entityManager,
     ) {
     }
 
@@ -29,14 +36,31 @@ class DumpSqlHandler
     {
         $filesystem = new Filesystem();
 
+        //$dumpSql->getOption());
 
-        try {
+        $abstractPlatform = $this->entityManager->getConnection()->getDatabasePlatform();
+        $schema = $this->entityManager->getConnection()->createSchemaManager();
+        $tables = $schema->introspectSchema()->getTables();
+
+        foreach ($tables as $table) {
+            echo $table->getName() . " columns:\n\n";
+            foreach ($table->getColumns() as $column) {
+                echo ' - ' . $column->getName() . "\n";
+            }
+        }
+        $sql = $abstractPlatform->getCreateTablesSQL($tables);
+        var_dump($sql);
+
+
+        /*try {
             $filesystem->mkdir(DatabaseManagerConst::ROOT_FOLDER_NAME);
         } catch (IOExceptionInterface $exception) {
             echo "An error occurred while creating your directory at ".$exception->getPath();
-        }
+        }*/
 
         $filesystem->appendToFile(DatabaseManagerConst::ROOT_FOLDER_NAME . 'logs.txt', 'Email sent to user@example.com', true);
+
+
 
         $user = $this->notificationService->findOneById(User::class, $dumpSql->getUserId());
         $this->notificationService->add($user, NotificationKey::NOTIFICATION_DUMP_SQL, ['file' => 'demo', 'url' => "#"]);
