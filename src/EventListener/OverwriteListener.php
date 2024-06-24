9e9937a0ff7d537dbf16cbd8d41eb0f8199f7cba
Aymeric <counteraccro@gmail.com>
2024-06-24 22:47:37 +0200

    Refonte code pour l'overwrite

--- src/EventListener/OverwriteListener.php
+++ src/EventListener/OverwriteListener.php
@@ -1,26 +1,67 @@
 <?php
+/**
+ * @author Gourdon Aymeric
+ * @version 1.0
+ * Listener qui va surcharger un controller en fonction de ce qui est défini dans overwrite.yaml
+ */
 
 namespace App\EventListener;
 
-use App\Controller\Admin\DashboardController;
-use App\Utils\Debug;
-use Symfony\Component\DependencyInjection\Attribute\AutowireLocator;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 use Symfony\Component\DependencyInjection\ServiceLocator;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpKernel\Event\ControllerEvent;
 use Symfony\Component\Routing\RouterInterface;
-use Symfony\Component\Security\Http\HttpUtils;
 use Symfony\Component\Yaml\Yaml;
 
 class OverwriteListener
 {
+    /**
+     * @var ServiceLocator
+     */
     private ServiceLocator $serviceLocator;
 
+    /**
+     * @var RouterInterface
+     */
     private RouterInterface $router;
 
+    /**
+     * Path fichier de config
+     * @var string
+     */
+    private const CONFIG_OVERWRITE_FILE = '..' . DIRECTORY_SEPARATOR . 'config' . DIRECTORY_SEPARATOR . 'cms' . DIRECTORY_SEPARATOR . 'overwrite.yaml';
+
+    /**
+     * Clé du fichier de config
+     * @var string
+     */
+    private const KEY_CONTROLLER = 'controller';
+
+    /**
+     * Clé du fichier de config
+     * @var string
+     */
+    private const KEY_ROUTE = 'route';
+
+    /**
+     * Clé du fichier de config
+     * @var string
+     */
+    private const KEY_OVERWRITE = 'overwrite';
+
+    /**
+     * Clé du fichier de config
+     * @var string
+     */
+    private const KEY_PARAMETERS = 'parameters';
+
+
+    /**
+     * @param ServiceLocator $serviceLocator
+     * @param RouterInterface $router
+     */
     public function __construct(
-        ServiceLocator $serviceLocator,
+        ServiceLocator  $serviceLocator,
         RouterInterface $router
     )
     {
@@ -28,47 +69,77 @@ class OverwriteListener
         $this->router = $router;
     }
 
+    /**
+     * Permet overwrite un controller en fonction de la config
+     * @param ControllerEvent $event
+     * @return void
+     */
     public function onControllerRequest(ControllerEvent $event): void
     {
-        $overwriteConfif = Yaml::parseFile('../config/cms/overwrite.yaml');
+
         if (!$event->isMainRequest()) {
             return;
         }
 
-
-        if(empty($overwriteConfif) || empty($overwriteConfif['overwrite']))
-        {
+        $tabOverwriteConfig = $this->loadOverwriteConfigFile();
+        if ($tabOverwriteConfig === null) {
             return;
         }
 
-        foreach($overwriteConfif['overwrite'] as $overwrite)
-        {
-            if($overwrite['controller'] === $event->getController()[0]::class
-                && $overwrite['route'] === $event->getRequest()->attributes->get('_route'))
-            {
-                $controller = $this->serviceLocator->get($overwrite['overwrite']['controller']);
-                $action = $overwrite['overwrite']['route'];
+        $tab = $this->getConfigByControllerAndRoute($event, $tabOverwriteConfig);
+
+        if ($tab[self::KEY_ROUTE] !== '') {
+            $route = $tab[self::KEY_ROUTE];
+            $params = $tab[self::KEY_PARAMETERS];
+            $event->setController(function () use ($route, $params) {
+                return new RedirectResponse($this->router->generate($route, $params));
+            });
+        }
+    }
+
+    /**
+     * Remonte une route et paramètres associés si ils existent dans la config
+     * @param ControllerEvent $event
+     * @param array $tabOverwriteConfig
+     * @return array
+     */
+    private function getConfigByControllerAndRoute(ControllerEvent $event, array $tabOverwriteConfig): array
+    {
+        $tabReturn = [
+            self::KEY_ROUTE => '',
+            self::KEY_PARAMETERS => [],
+        ];
+
+        foreach ($tabOverwriteConfig[self::KEY_OVERWRITE] as $row) {
+            if ($row[self::KEY_CONTROLLER] === $event->getController()[0]::class
+                && $row[self::KEY_ROUTE] === $event->getRequest()->attributes->get('_route')) {
+                $tabReturn[self::KEY_ROUTE] = $row[self::KEY_OVERWRITE][self::KEY_ROUTE];
                 $params = $event->getRequest()->attributes->get('_route_params');
 
-                $newParams = [];
-                if(!empty($params))
-                {
+                if (!empty($params)) {
                     foreach ($params as $key => $value) {
-                        if(isset($overwrite['overwrite']['parameters'][$key]))
-                        {
-                            $newParams[$overwrite['overwrite']['parameters'][$key]] = $value;
+                        if (isset($row[self::KEY_OVERWRITE][self::KEY_PARAMETERS][$key])) {
+                            $tabReturn[self::KEY_PARAMETERS][$row[self::KEY_OVERWRITE][self::KEY_PARAMETERS][$key]] = $value;
                         }
                     }
                 }
-
-                var_dump($newParams);
-
-                //Debug::printR($this->router->getRouteCollection());
-
-                $event->setController(function () use ($action, $newParams) {
-                    return  new RedirectResponse($this->router->generate($action, $newParams));
-                });
             }
         }
+        return $tabReturn;
+    }
+
+    /**
+     * Retourne le fichier de config Overwrite
+     * @return array|null
+     */
+    private function loadOverwriteConfigFile(): array|null
+    {
+        $tabOverwriteConfig = Yaml::parseFile(self::CONFIG_OVERWRITE_FILE);
+
+        if (!isset($tabOverwriteConfig[self::KEY_OVERWRITE]) || empty($tabOverwriteConfig[self::KEY_OVERWRITE])) {
+            return null;
+        }
+
+        return $tabOverwriteConfig;
     }
 }
