03bd213646fa4836b5c10ae3bd107c5c8f294a3f
Aymeric <counteraccro@gmail.com>
2024-08-08 09:44:19 +0200

    Avancement sur le rendu des menus

--- assets/vue/Components/Menu/MenuLeftRight.vue
+++ assets/vue/Components/Menu/MenuLeftRight.vue
@@ -12,7 +12,8 @@ export default {
   props: {
     menu: Object,
     type: Number,
-    data: Object
+    data: Object,
+    locale: String
   },
   watch: {
     type: 'switchType'
@@ -51,6 +52,7 @@ export default {
      * @param tabMenuElementTranslation
      */
     getTranslationByLocale(tabMenuElementTranslation) {
+      console.log(tabMenuElementTranslation)
       return MenuElementTools.getTranslationByLocale(tabMenuElementTranslation, this.locale);
     },
 
@@ -62,58 +64,64 @@ export default {
       return menuElement.hasOwnProperty('children');
     },
 
+    /**
+     * Génère les profondeurs de deepMenu
+     * @param menuElement
+     * @param html
+     * @return string
+     */
+    renderCollapse(menuElement, html) {
+
+      menuElement.children.forEach((element) => {
+        let elementTranslate = this.getTranslationByLocale(element.menuElementTranslations);
+        if (!element.disabled) {
+          if (!this.isHaveChildren(element)) {
+            html += '<li><a class="link-body-emphasis d-inline-flex text-decoration-none rounded element-hover"  target="' + element.linkTarget + '" href="' + elementTranslate.link + '">' + elementTranslate.text + '</a>'
+          } else {
+            html += '<li><a href="#" class="link-body-emphasis d-inline-flex text-decoration-none rounded link-toggle element-hover no-control" data-bs-toggle="collapse" data-bs-target="#demo-' + element.id + '" aria-expanded="false"> ' + elementTranslate.text + '</a>'
+            html += '<div class="collapse" id="demo-' + element.id + '">';
+            html += '<ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small">'
+            html = this.renderCollapse(element, html);
+            html += '</ul></div>';
+          }
+          html += '</li>';
+        }
+      });
+
+      return html;
+    },
+
   }
 }
 </script>
 
 <template>
 
-  <div class="d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary" :class="this.definePosition()" style="width: 280px;">
-    <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
-      <i class="bi pe-none me-2 h5" :class="this.data.logo"></i>
-      <span class="fs-4">{{ this.data.name }}</span>
+  <div class="flex-shrink-0 p-3 bg-body-tertiary border border-1 border-light" :class="this.definePosition()" style="width: 280px;">
+    <a :href="this.data.ur_site" class="d-flex align-items-center pb-3 mb-3 link-body-emphasis text-decoration-none border-bottom">
+      <i class="bi pe-none me-2 h4 mt-2" :class="this.data.logo"></i>
+      <span class="fs-5 fw-semibold">{{ this.data.name }}</span>
     </a>
-    <hr>
-    <ul class="nav nav-pills flex-column mb-auto">
-      <li class="nav-item">
-        <a href="#" class="nav-link active" aria-current="page">
-          <svg class="bi pe-none me-2" width="16" height="16">
-            <use xlink:href="#home"></use>
-          </svg>
-          Home
+
+    <ul v-if="this.type === 11" class="list-unstyled ps-0">
+      <li v-for="(element) in this.menu.menuElements" class="mb-1" :set="elementTranslate = this.getTranslationByLocale(element.menuElementTranslations)">
+        <a v-if="!element.disabled" class="btn d-inline-flex align-items-center rounded border-0 collapsed element-hover"
+            :target="element.linkTarget" :href="elementTranslate.link">{{ elementTranslate.text }}
         </a>
       </li>
-      <li>
-        <a href="#" class="nav-link link-body-emphasis">
-          <svg class="bi pe-none me-2" width="16" height="16">
-            <use xlink:href="#speedometer2"></use>
-          </svg>
-          Dashboard
+    </ul>
+
+    <ul v-if="this.type === 12" class="list-unstyled ps-0">
+      <li v-for="(element) in this.menu.menuElements" class="mb-1" :set="elementTranslate = this.getTranslationByLocale(element.menuElementTranslations)">
+        <a v-if="!this.isHaveChildren(element) && !element.disabled" class="btn d-inline-flex align-items-center rounded border-0 collapsed element-hover"
+            :target="element.linkTarget" :href="elementTranslate.link">{{ elementTranslate.text }}
         </a>
-      </li>
-      <li>
-        <a href="#" class="nav-link link-body-emphasis">
-          <svg class="bi pe-none me-2" width="16" height="16">
-            <use xlink:href="#table"></use>
-          </svg>
-          Orders
-        </a>
-      </li>
-      <li>
-        <a href="#" class="nav-link link-body-emphasis">
-          <svg class="bi pe-none me-2" width="16" height="16">
-            <use xlink:href="#grid"></use>
-          </svg>
-          Products
-        </a>
-      </li>
-      <li>
-        <a href="#" class="nav-link link-body-emphasis">
-          <svg class="bi pe-none me-2" width="16" height="16">
-            <use xlink:href="#people-circle"></use>
-          </svg>
-          Customers
-        </a>
+        <button v-else-if="!element.disabled" class="btn btn-toggle d-inline-flex align-items-center rounded border-0 collapsed" data-bs-toggle="collapse" :data-bs-target="'#demo-' + element.id" aria-expanded="false">
+          {{ elementTranslate.text }}
+        </button>
+        <div v-if="this.isHaveChildren(element)" :id="'demo-' + element.id" class="collapse">
+          <ul class="btn-toggle-nav list-unstyled fw-normal pb-1 small" v-html="this.renderCollapse(element, '')"></ul>
+        </div>
       </li>
     </ul>
   </div>
