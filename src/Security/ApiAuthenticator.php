a9cefca91b00199a7ca6a074e6b8947f36be844e
Aymeric <counteraccro@gmail.com>
2024-09-14 19:03:34 +0200

    Mise en place filtre IP

--- src/Security/ApiAuthenticator.php
+++ src/Security/ApiAuthenticator.php
@@ -1,5 +1,9 @@
 <?php
-
+/**
+ * @author Gourdon Aymeric
+ * @version 1.0
+ * authenticate pour API
+ */
 namespace App\Security;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -15,13 +19,23 @@ use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPasspor
 class ApiAuthenticator extends AbstractAuthenticator
 {
 
+    /**
+     * @param Request $request
+     * @return bool|null
+     */
     public function supports(Request $request): ?bool
     {
         return $request->headers->has('Authorization') && str_contains($request->headers->get('Authorization'), 'Bearer ');
     }
 
+    /**
+     * @param Request $request
+     * @return Passport
+     */
     public function authenticate(Request $request): Passport
     {
+        var_dump($request->getClientIp());
+        var_dump($_SERVER['SERVER_ADDR']);
         $identifier = str_replace('Bearer ', '', $request->headers->get('Authorization'));
 
         return new SelfValidatingPassport(
@@ -37,6 +51,7 @@ class ApiAuthenticator extends AbstractAuthenticator
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception): ?Response
     {
         return new JsonResponse([
+            'toto' => 'ici',
             'message' => strtr($exception->getMessageKey(), $exception->getMessageData())
         ], Response::HTTP_UNAUTHORIZED);
     }
--- src/Service/Api/ApiTokenService.php
+++ src/Service/Api/ApiTokenService.php
@@ -4,6 +4,7 @@
  * @version 1.1
  * Service pour ApiToken
  */
+
 namespace App\Service\Api;
 
 use App\Entity\Admin\System\User;
@@ -13,8 +14,32 @@ use Psr\Container\NotFoundExceptionInterface;
 
 class ApiTokenService extends AppApiService
 {
+
     /**
-     * Converti un objet ApiToken en User pour le ApiProvider
+     * Détermine si on peut accéder à l'API ou non
+     * @return bool
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    private function canAccessToApi(): bool
+    {
+        $ipClient = $this->getRequestStack()->getCurrentRequest()->getClientIp();
+        $ip = $this->getParameterBag()->get('app.ip_api_authorize');
+        $filter = $this->getParameterBag()->get('app.ip_api_active_filter');
+
+        if (!$filter) {
+            return true;
+        }
+
+        if (!in_array($ipClient, $ip)) {
+            return false;
+        }
+        return true;
+    }
+
+    /**
+     * Converti un objet ApiToken en User pour le ApiProvider Si les conditions suivantes sont remplis
+     *  - IP autorisée, Token valide
      * @param string $token
      * @return User|null
      * @throws ContainerExceptionInterface
@@ -22,10 +47,13 @@ class ApiTokenService extends AppApiService
      */
     public function getUserByApiToken(string $token): ?User
     {
+        if(!$this->canAccessToApi()) {
+            return null;
+        }
+
         /** @var ApiToken $apiToken */
         $apiToken = $this->findOneBy(ApiToken::class, 'token', $token);
-        if($apiToken === null)
-        {
+        if ($apiToken === null) {
             return null;
         }
 
