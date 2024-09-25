f56c5a0795feb217cb4871f7515e0c0d0a0dbaed
Aymeric <counteraccro@gmail.com>
2024-09-25 11:15:22 +0200

    Mise en place test token write et token admin

--- tests/Api/AuthenticationTest.php
+++ tests/Api/AuthenticationTest.php
@@ -38,4 +38,36 @@ class AuthenticationTest extends AppApiTest
         $this->assertJson($response->getContent());
         $this->assertContains('ROLE_READ_API', json_decode($response->getContent(), true)['data']['roles'], 'ROLE_READ_API non présent');
     }
+
+    /**
+     * Test le token d'accès en écriture
+     * @return void
+     */
+    public function testWriteToken(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', self::URL_REF . '/authentication',
+            server: $this->getCustomHeaders(self::HEADER_WRITE)
+        );
+        $response = $client->getResponse();
+        $this->assertEquals(200, $response->getStatusCode(), 'Le token doit être valide');
+        $this->assertJson($response->getContent());
+        $this->assertContains('ROLE_WRITE_API', json_decode($response->getContent(), true)['data']['roles'], 'ROLE_WRITE_API non présent');
+    }
+
+    /**
+     * Test le token d'accès en admin
+     * @return void
+     */
+    public function testAdminToken(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', self::URL_REF . '/authentication',
+            server: $this->getCustomHeaders(self::HEADER_ADMIN)
+        );
+        $response = $client->getResponse();
+        $this->assertEquals(200, $response->getStatusCode(), 'Le token doit être valide');
+        $this->assertJson($response->getContent());
+        $this->assertContains('ROLE_ADMIN_API', json_decode($response->getContent(), true)['data']['roles'], 'ROLE_ADMIN_API non présent');
+    }
 }
