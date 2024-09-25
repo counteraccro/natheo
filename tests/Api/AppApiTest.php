0dbc2a9992af33cd9f5ef2ce3200c0b8faaaf3f1
Aymeric <counteraccro@gmail.com>
2024-09-25 11:18:13 +0200

    Clean code

--- tests/Api/AppApiTest.php
+++ tests/Api/AppApiTest.php
@@ -1,21 +1,51 @@
 <?php
-
+/**
+ * Class global pour les test des API
+ * @author Gourdon Aymeric
+ * @version 1.0
+ */
 namespace Api;
 
 use App\Utils\System\ApiToken\ApiTokenConst;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
-use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
-use function Symfony\Component\String\b;
 
 class AppApiTest extends WebTestCase
 {
 
-    protected const API_VERSION = '1';
+    /**
+     * Version API
+     * @var int
+     */
+    protected const API_VERSION = 1;
+
+    /**
+     * URL API pour les tests
+     * @var string
+     */
     protected const URL_REF = 'http://dev.natheo/api/' . self::API_VERSION;
 
+    /**
+     * Code pour générer un header avec un mauvais token
+     * @var int
+     */
     protected const HEADER_WRONG = 1;
+
+    /**
+     * Code pour générer un header avec un token accès lecture
+     * @var int
+     */
     protected const HEADER_READ = 2;
+
+    /**
+     * Code pour générer un header avec un token accès écriture
+     * @var int
+     */
     protected const HEADER_WRITE = 3;
+
+    /**
+     * Code pour générer un header avec un token accès admin
+     * @var int
+     */
     protected const HEADER_ADMIN = 4;
 
     /**
