e21c4c3f3ade995bf34420dc86d9948fa9ec5580
Aymeric <counteraccro@gmail.com>
2024-10-18 07:36:04 +0200

    Préparation de l'API pour les page

---
+++ src/Controller/Api/v1/Content/ApiPageController.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * @author Gourdon Aymeric
+ * @version 1.0
+ * Controller pour les pages via API
+ */
+namespace App\Controller\Api\v1\Content;
+
+use App\Controller\Api\v1\AppApiController;
+use App\Utils\Api\ApiConst;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+#[Route('/api/{api_version}/page', name: 'api_page_', requirements: ['_version' => '%app.api_version%'])]
+#[IsGranted("ROLE_READ_API")]
+class ApiPageController extends AppApiController
+{
+    /**
+     * Retourne une page en fonction de son slug
+     * @return JsonResponse
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    #[Route('/find', name: 'find')]
+    public function index(): JsonResponse
+    {
+        return $this->apiResponse(ApiConst::API_MSG_SUCCESS, ['menu' => '']);
+    }
+}
