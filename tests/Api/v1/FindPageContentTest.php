2c9ce23b440065abffd34b5b18e6c5187d2279f6
Aymeric <counteraccro@gmail.com>
2024-11-08 07:39:15 +0100

    Mise à jour des tests

---
+++ tests/Api/v1/FindPageContentTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Tests\Api\v1;
+
+use Api\v1\AppApiTest;
+
+class FindPageContentTest extends AppApiTest
+{
+    private const URL_FIND_PAGE_CONTENT = self::URL_REF . '/page/content';
+
+    /**
+     * Test une mauvaise locale
+     * @return void
+     */
+    public function testWrongParameter(): void
+    {
+        $parameters = "?id=123&locale=er";
+
+        $client = static::createClient();
+        $client->request('GET', self::URL_FIND_PAGE_CONTENT . $parameters,
+            server: $this->getCustomHeaders(self::HEADER_READ)
+        );
+        $response = $client->getResponse();
+
+        $this->assertEquals(403, $response->getStatusCode());
+        $this->assertJson($response->getContent());
+    }
+
+    /**
+     * Test avec id qui n'existe pas
+     * @return void
+     */
+    public function testBadId(): void
+    {
+        $parameters = "?id=0&locale=fr";
+        $client = static::createClient();
+        $client->request('GET', self::URL_FIND_PAGE_CONTENT . $parameters,
+            server: $this->getCustomHeaders(self::HEADER_READ)
+        );
+        $response = $client->getResponse();
+
+        $this->assertEquals(403, $response->getStatusCode());
+        $this->assertJson($response->getContent());
+    }
+
+
+
+    /**
+     * Test good id
+     * @return void
+     */
+    public function testSlug()
+    {
+        $parameters = "?id=85";
+        $client = static::createClient();
+        $client->request('GET', self::URL_FIND_PAGE_CONTENT . $parameters,
+            server: $this->getCustomHeaders(self::HEADER_READ)
+        );
+        $response = $client->getResponse();
+
+        $this->assertEquals(200, $response->getStatusCode());
+        $this->assertJson($response->getContent());
+    }
+}
