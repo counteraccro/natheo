67908eb52ed508645859a2bb0638facf1b095de6
Aymeric <counteraccro@gmail.com>
2024-11-21 07:34:41 +0100

    Avancement commande installation

--- src/Command/InstallCommand.php
+++ src/Command/InstallCommand.php
@@ -2,12 +2,17 @@
 
 namespace App\Command;
 
+use App\Service\Installation\InstallationService;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
@@ -18,7 +23,8 @@ use Symfony\Contracts\Translation\TranslatorInterface;
 class InstallCommand extends Command
 {
     public function __construct(
-        private TranslatorInterface $translator
+        private TranslatorInterface $translator,
+        private InstallationService $installationService
     )
     {
         parent::__construct();
@@ -32,6 +38,11 @@ class InstallCommand extends Command
         ;*/
     }
 
+    /**
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     * @throws \Throwable
+     */
     protected function execute(InputInterface $input, OutputInterface $output): int
     {
         $io = new SymfonyStyle($input, $output);
@@ -44,19 +55,99 @@ class InstallCommand extends Command
             $this->translator->trans('install.description.create.schema', domain: 'command'),
             $this->translator->trans('install.description.fixtures.load', domain: 'command'),
             $this->translator->trans('install.description.clear.cache', domain: 'command'),
-            ]);
+        ]);
 
-        //$arg1 = $input->getArgument('arg1');
-
-        /*if ($arg1) {
-            $io->note(sprintf('You passed an argument: %s', $arg1));
+        $delete = false;
+        if ($this->installationService->checkSchema()) {
+            $delete = true;
+            $io->warning($this->translator->trans('install.description.schema.warning', domain: 'command'));
         }
 
-        if ($input->getOption('option1')) {
-            $io->note(sprintf('You passed an option: %s', $input->getOption('option1')));
-        }*/
+        if (!$io->confirm($this->translator->trans('install.description.confirm', domain: 'command'), false)) {
+            $io->text($this->translator->trans('install.description.confirm.no', domain: 'command'));
+            return Command::SUCCESS;
+        }
 
-        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+        // Drop database
+        if ($delete) {
+            $io->title($this->translator->trans('install.drop.database', domain: 'command'));
+
+            $commandInput = new ArrayInput([
+                'command' => 'doctrine:database:drop',
+                '--force' => true,
+            ]);
+
+            $returnCode = $this->getApplication()->doRun($commandInput, $output);
+            if ($returnCode === 0) {
+                $io->text($this->translator->trans('install.drop.database.success', domain: 'command'));
+            } else {
+                $io->text($this->translator->trans('install.error', domain: 'command'));
+                return Command::FAILURE;
+            }
+        }
+
+        // Create database
+        $io->title($this->translator->trans('install.create.database', domain: 'command'));
+
+        $commandInput = new ArrayInput([
+            'command' => 'doctrine:database:create',
+        ]);
+
+        $returnCode = $this->getApplication()->doRun($commandInput, $output);
+        if ($returnCode === 0) {
+            $io->text($this->translator->trans('install.create.database.success', domain: 'command'));
+        } else {
+            $io->text($this->translator->trans('install.error', domain: 'command'));
+            return Command::FAILURE;
+        }
+
+        // Create schema
+        $io->title($this->translator->trans('install.create.schema', domain: 'command'));
+
+        $commandInput = new ArrayInput([
+            'command' => 'doctrine:schema:create',
+        ]);
+
+        $returnCode = $this->getApplication()->doRun($commandInput, $output);
+        if ($returnCode === 0) {
+            $io->text($this->translator->trans('install.create.schema.success', domain: 'command'));
+        } else {
+            $io->text($this->translator->trans('install.error', domain: 'command'));
+            return Command::FAILURE;
+        }
+
+        // Create fixtures
+        $io->title($this->translator->trans('install.fixture.load', domain: 'command'));
+
+        $commandInput = new ArrayInput([
+            'command' => 'doctrine:fixtures:load',
+            '--append' => true
+        ]);
+
+        $returnCode = $this->getApplication()->doRun($commandInput, $output);
+        if ($returnCode === 0) {
+            $io->text($this->translator->trans('install.fixture.load.success', domain: 'command'));
+        } else {
+            $io->text($this->translator->trans('install.error', domain: 'command'));
+            return Command::FAILURE;
+        }
+
+        // clear cache
+        $io->title($this->translator->trans('install.clear.cache', domain: 'command'));
+
+        $commandInput = new ArrayInput([
+            'command' => 'cache:clear',
+        ]);
+
+        $returnCode = $this->getApplication()->doRun($commandInput, $output);
+        if ($returnCode === 0) {
+            $io->text($this->translator->trans('install.clear.cache.success', domain: 'command'));
+        } else {
+            $io->text($this->translator->trans('install.error', domain: 'command'));
+            return Command::FAILURE;
+        }
+
+        $io->success($this->translator->trans('install.success', domain: 'command'));
 
         return Command::SUCCESS;
     }
