e44db6a0acf4cd3405439c652f9633b3c7796bfd
Aymeric <counteraccro@gmail.com>
2025-01-04 14:25:05 +0100

    Avancement recherche

--- src/Service/Admin/GlobalSearchService.php
+++ src/Service/Admin/GlobalSearchService.php
@@ -8,6 +8,7 @@
 namespace App\Service\Admin;
 
 use App\Entity\Admin\Content\Page\Page;
+use App\Utils\Content\Page\PageConst;
 use Doctrine\ORM\Tools\Pagination\Paginator;
 use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\NotFoundExceptionInterface;
@@ -36,7 +37,7 @@ class GlobalSearchService extends AppAdminService
         $repository = $this->getRepository(ucfirst($entity));
         $result = $repository->search($search, $page, $limit);
 
-        return $this->formatResult($result);
+        return $this->formatResult($result, $entity, $search);
     }
 
     /**
@@ -52,24 +53,79 @@ class GlobalSearchService extends AppAdminService
         };
     }
 
+
     /**
+     * @param Paginator $paginator
+     * @param string $entity
+     * @return array
      * @throws ContainerExceptionInterface
      * @throws NotFoundExceptionInterface
      */
-    private function formatResult(Paginator $paginator): array
+    private function formatResult(Paginator $paginator, string $entity, string $search): array
     {
-        $locales = $this->getLocales();
-
         $return = ['elements' => [], 'total' => $paginator->count()];
+        $locales = $this->getLocales();
         foreach ($paginator as $item) {
-
-            /** @var Page $item */
-            $return['elements'][] = [
-                'id' => $item->getId(),
-                'label' => $item->getPageTranslationByLocale($locales['current'])->getTitre(),
-            ];
+            switch ($entity) {
+                case Page::class:
+                    /** @var Page $item */
+                    $return['elements'][] = $this->formatResulPage($item, $locales['current'], $search);
+                    break;
+                default:
+            }
         }
 
         return $return;
+    }
+
+    /**
+     * @param Page $page
+     * @param string $locale
+     * @param string $search
+     * @return array
+     */
+    private function formatResulPage(Page $page, string $locale, string $search): array
+    {
+        $label = $page->getPageTranslationByLocale($locale)->getTitre();
+        $label = $this->highlightText($search, $label);
+
+        $content = [];
+        foreach ($page->getPageContents() as $pageContent) {
+            if ($pageContent->getType() === PageConst::CONTENT_TYPE_TEXT) {
+                $text = $pageContent->getPageContentTranslationByLocale($locale)->getText();
+                $re = '/\b((?:\w+[^\w\n]+){0,2}' . $search . '\b(?:[^\w\n]+\w+){0,2})/mu';
+                preg_match_all($re, $text, $matches, PREG_SET_ORDER, 0);
+
+                foreach($matches as $matche)
+                {
+                    $content[] = $this->highlightText($search, $matche[0]);
+                }
+
+                /*if (str_contains($text, $search)) {
+                    $start = strpos($text, $search) - 25;
+                    $end = strlen($search) + 25;
+                    $text = substr($text, $start, $end);
+                    $content[] = $this->highlightText($search, $text);
+                }*/
+            }
+        }
+
+        $return = [
+            'id' => $page->getId(),
+            'label' => $label,
+            'contents' => $content,
+        ];
+        return $return;
+    }
+
+    /**
+     * Surligne un élement de recherche
+     * @param string $search
+     * @param string $text
+     * @return string
+     */
+    private function highlightText(string $search, string $text): string
+    {
+        return str_replace($search, '<mark>' . $search . '</mark>', $text);
     }
 }
