cc8c34f4832bca09420602d71db5e6f1b9a16886
Aymeric <counteraccro@gmail.com>
2025-02-18 08:10:59 +0100

    déplacement fichier

---
+++ tests/Helper/Fixtures/System/UserFixturesTrait.php
@@ -0,0 +1,122 @@
+<?php
+/**
+ * @author Gourdon Aymeric
+ * @version 1.0
+ * Fixture text user
+ */
+
+namespace App\Tests\Helper\Fixtures\System;
+
+use App\Entity\Admin\System\User;
+use App\Tests\Helper\FakerTrait;
+
+trait UserFixturesTrait
+{
+    use FakerTrait;
+
+    /**
+     * Créer un utilisateur de role USER
+     * @param array $customData
+     * @param bool $persist
+     * @return User
+     */
+    public function createUser(array $customData = [], bool $persist = true): User
+    {
+        $data = [
+            'email' => self::getFaker()->email(),
+            'roles' => ['ROLE_USER'],
+            'password' => self::getFaker()->password(),
+            'login' => self::getFaker()->username(),
+            'firstname' => self::getFaker()->firstName(),
+            'lastname' => self::getFaker()->lastName(),
+            'disabled' => self::getFaker()->boolean(),
+            'anonymous' => self::getFaker()->boolean(),
+            'founder' => false,
+        ];
+
+        $user = $this->initEntity(User::class, array_merge($data, $customData));
+
+        if ($persist) {
+            $this->persistAndFlush($user);
+        }
+        return $user;
+    }
+
+    /**
+     * Créer un utilisateur de role Contributeur
+     * @param array $customData
+     * @param bool $persist
+     * @return User
+     */
+    public function createUserContributeur(array $customData = [], bool $persist = true): User
+    {
+        $data = [
+            'roles' => ['ROLE_CONTRIBUTEUR'],
+        ];
+
+        $user = $this->createUser(array_merge($data, $customData), false);
+        if ($persist) {
+            $this->persistAndFlush($user);
+        }
+        return $user;
+    }
+
+    /**
+     * Créer un utilisateur de role admin
+     * @param array $customData
+     * @param bool $persist
+     * @return User
+     */
+    public function createUserAdmin(array $customData = [], bool $persist = true): User
+    {
+        $data = [
+            'roles' => ['ROLE_ADMIN'],
+        ];
+
+        $user = $this->createUser(array_merge($data, $customData), false);
+        if ($persist) {
+            $this->persistAndFlush($user);
+        }
+        return $user;
+    }
+
+    /**
+     * Créer un utilisateur de role superAdmin
+     * @param array $customData
+     * @param bool $persist
+     * @return User
+     */
+    public function createUserSuperAdmin(array $customData = [], bool $persist = true): User
+    {
+        $data = [
+            'roles' => ['ROLE_SUPER_ADMIN'],
+        ];
+
+        $user = $this->createUser(array_merge($data, $customData), false);
+        if ($persist) {
+            $this->persistAndFlush($user);
+        }
+        return $user;
+    }
+
+    /**
+     * Créer un user fondateur
+     * @param array $customData
+     * @param bool $persist
+     * @return User
+     */
+    public function createUserFounder(array $customData = [], bool $persist = true): User
+    {
+        $data = [
+            'roles' => ['ROLE_SUPER_ADMIN'],
+            'founder' => true,
+        ];
+
+        $user = $this->createUser(array_merge($data, $customData), false);
+
+        if ($persist) {
+            $this->persistAndFlush($user);
+        }
+        return $user;
+    }
+}
