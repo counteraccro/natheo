c475268447c4f672324583d1e040d838ee340bb4
Aymeric <counteraccro@gmail.com>
2025-02-19 08:03:56 +0100

    Ajout nouvelle fixtures

---
+++ tests/Helper/Fixtures/System/OptionUserFixturesTrait.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Tests\Helper\Fixtures\System;
+
+use App\Entity\Admin\System\OptionUser;
+use App\Entity\Admin\System\User;
+use App\Tests\Helper\FakerTrait;
+use App\Utils\System\Options\OptionUserKey;
+
+trait OptionUserFixturesTrait
+{
+    use FakerTrait;
+
+    /**
+     * Créer une option user
+     * @param User $user
+     * @param array $customData
+     * @param bool $persist
+     * @return OptionUser
+     */
+    public function createOptionUser(User $user, array $customData = [], bool $persist = true): OptionUser
+    {
+        $data = [
+            'user' => $user,
+            'key' => self::getFaker()->text(),
+            'value' => self::getFaker()->text(),
+        ];
+
+        $option = $this->initEntity(OptionUser::class, array_merge($data, $customData));
+        if ($persist) {
+            $this->persistAndFlush($option);
+        }
+        return $option;
+    }
+
+    /**
+     * Génère les options par défauts
+     * @param User $user
+     * @return void
+     */
+    public function generateDefaultOptionUser(User $user): void
+    {
+        $data = [
+            'key' => OptionUserKey::OU_THEME_SITE,
+            'value' => 'purple'
+        ];
+        $this->createOptionUser($user, $data);
+
+        $data = [
+            'key' => OptionUserKey::OU_DEFAULT_LANGUAGE,
+            'value' => 'fr'
+        ];
+        $this->createOptionUser($user, $data);
+
+        $data = [
+            'key' => OptionUserKey::OU_NB_ELEMENT,
+            'value' => '20'
+        ];
+        $this->createOptionUser($user, $data);
+
+        $data = [
+            'key' => OptionUserKey::OU_DEFAULT_PERSONAL_DATA_RENDER,
+            'value' => 'email'
+        ];
+        $this->createOptionUser($user, $data);
+    }
+}
