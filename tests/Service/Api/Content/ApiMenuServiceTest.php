8a72d09ec6b3677822ac490bea74442c027bb367
Aymeric <counteraccro@gmail.com>
2025-05-23 07:51:24 +0200

    Ajout test sur API

--- tests/Service/Api/Content/ApiMenuServiceTest.php
+++ tests/Service/Api/Content/ApiMenuServiceTest.php
@@ -7,14 +7,17 @@
 
 namespace Service\Api\Content;
 
+use App\Dto\Api\Content\Menu\ApiFindMenuDto;
 use App\Service\Api\Content\ApiMenuService;
 use App\Tests\AppWebTestCase;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
 use Symfony\Component\DependencyInjection\Container;
 
 class ApiMenuServiceTest extends AppWebTestCase
 {
     /**
-     *@var ApiMenuService|mixed|object|Container|null
+     * @var ApiMenuService|mixed|object|Container|null
      */
     private ApiMenuService $apiMenuService;
 
@@ -27,9 +30,31 @@ class ApiMenuServiceTest extends AppWebTestCase
     /**
      * test méthode getMenuForApi()
      * @return void
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
      */
-    public function testGetMenuForApi() :void
+    public function testGetMenuForApi(): void
     {
-        //$this->apiMenuService->
+        $menu = $this->createMenuAllDataDefault();
+
+        $dto = new ApiFindMenuDto($menu->getId(), '', 1, 'fr', '');
+
+        $nb = 0;
+        foreach($menu->getMenuElements() as $menuElement) {
+            if($menuElement->getParent() === null && !$menuElement->isDisabled()) {
+                $nb++;
+            }
+        }
+
+        $result = $this->apiMenuService->getMenuForApi($dto);
+        $this->assertIsArray($result);
+        $this->assertArrayHasKey('id', $result);
+        $this->assertEquals($menu->getId(), $result['id']);
+        $this->assertArrayHasKey('position', $result);
+        $this->assertEquals('HEADER', $result['position']);
+        $this->assertArrayHasKey('type', $result);
+        $this->assertEquals($menu->getType(), $result['type']);
+        $this->assertArrayHasKey('elements', $result);
+        $this->assertCount($nb, $result['elements']);
     }
 }
