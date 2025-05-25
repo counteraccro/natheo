204d3a8f524bcb367f2934fe7e4ac17e4ac867e2
Aymeric <counteraccro@gmail.com>
2025-05-25 09:22:25 +0200

    Mise en place API optionSystem

--- src/Controller/Api/v1/System/ApiOptionSystemController.php
+++ src/Controller/Api/v1/System/ApiOptionSystemController.php
@@ -2,16 +2,20 @@
 /**
  * @author Gourdon Aymeric
  * @version 1.0
- *
+ *  Api Controller pour les options systems
  */
 
 namespace App\Controller\Api\v1\System;
 
 use App\Controller\Api\v1\AppApiController;
+use App\Service\Admin\System\OptionSystemService;
+use App\Service\Api\System\ApiOptionSystemService;
 use App\Utils\Api\ApiConst;
 use Psr\Container\ContainerExceptionInterface;
 use Psr\Container\NotFoundExceptionInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
 
@@ -21,14 +25,37 @@ class ApiOptionSystemController extends AppApiController
 {
 
     /**
+     * Retourne la liste des optionsSystems pour le front
+     * @param ApiOptionSystemService $apiOptionSystemService
      * @return JsonResponse
      * @throws ContainerExceptionInterface
      * @throws NotFoundExceptionInterface
      */
-    #[Route('/', name: 'listing', methods: ['GET'])]
-    public function listing(): jsonResponse
+    #[Route('', name: 'listing', methods: ['GET'])]
+    public function listing(ApiOptionSystemService $apiOptionSystemService): jsonResponse
     {
+        return $this->apiResponse(ApiConst::API_MSG_SUCCESS, $apiOptionSystemService->getAllOptionSystemWithValue());
+    }
 
-        return $this->apiResponse(ApiConst::API_MSG_SUCCESS, []);
+    /**
+     * Retourne une optionSystem en fonction de sa clé
+     * @param ApiOptionSystemService $apiOptionSystemService
+     * @param OptionSystemService $optionSystemService
+     * @param string|null $key
+     * @return JsonResponse
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    #[Route('/{key}', name: 'get_by_key', methods: ['GET'])]
+    public function getByKey(
+        ApiOptionSystemService $apiOptionSystemService,
+        OptionSystemService    $optionSystemService,
+        ?string                $key = null): JsonResponse
+    {
+        if (!in_array($key, $apiOptionSystemService->getWhiteListeOptionSystem())) {
+            $translator = $this->getTranslator();
+            throw new HttpException(Response::HTTP_FORBIDDEN, $translator->trans('api_errors.find.option.system.not.found', domain: 'api_errors'));
+        }
+        return $this->apiResponse(ApiConst::API_MSG_SUCCESS, ['key' => $key, 'value' => $optionSystemService->getValueByKey($key)]);
     }
 }
---
+++ src/Service/Api/System/ApiOptionSystemService.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @author Gourdon Aymeric
+ * @version 1.0
+ * Service ApiOptionSystem
+ */
+
+namespace App\Service\Api\System;
+
+use App\Service\Api\AppApiService;
+use App\Utils\System\Options\OptionSystemKey;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
+
+class ApiOptionSystemService extends AppApiService
+{
+    /**
+     * Retourne un tableau de clé d'options system autorisée pour l'API
+     * @return array
+     */
+    public function getWhiteListeOptionSystem(): array
+    {
+        return [
+            OptionSystemKey::OS_SITE_NAME, OptionSystemKey::OS_FRONT_SCRIPT_TOP,
+            OptionSystemKey::OS_FRONT_SCRIPT_START_BODY, OptionSystemKey::OS_FRONT_SCRIPT_END_BODY,
+            OptionSystemKey::OS_OPEN_COMMENT, OptionSystemKey::OS_ADRESSE_SITE
+        ];
+    }
+
+    /**
+     * Retourne la liste des options system avec la value
+     * @return array
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    public function getAllOptionSystemWithValue(): array
+    {
+        $optionSystemService = $this->getOptionSystemService();
+
+        $return = [];
+        foreach ($this->getWhiteListeOptionSystem() as $option) {
+            $return[$option] = $optionSystemService->getValueByKey($option);
+        }
+
+        return $return;
+    }
+}
