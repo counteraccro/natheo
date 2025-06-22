8a6748bc587702c81acd6c6f8eab5f46577e7a80
Aymeric <counteraccro@gmail.com>
2025-06-22 09:24:22 +0200

    Architecture

---
+++ src/Service/Front/AppFrontService.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * @author Gourdon Aymeric
+ * @version 1.0
+ * AppService pour le front
+ */
+
+namespace App\Service\Front;
+
+use App\Utils\System\Options\OptionSystemKey;
+use Psr\Container\ContainerExceptionInterface;
+use Psr\Container\NotFoundExceptionInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+
+class AppFrontService extends AppFrontHandlerService
+{
+    /**
+     * Retourne le path du template en fonction de l'option system OS_THEME_FRONT_SITE
+     * @return string
+     * @throws ContainerExceptionInterface
+     * @throws NotFoundExceptionInterface
+     */
+    public function getPathTemplate(): string
+    {
+        $translator = $this->getTranslator();
+        $optionSystemService = $this->getOptionSystemService();
+        $template = $optionSystemService->getValueByKey(OptionSystemKey::OS_THEME_FRONT_SITE);
+
+        $check = ['natheo_horizon', 'front_storm'];
+        if (!in_array($template, $check)) {
+            throw new HttpException(Response::HTTP_INTERNAL_SERVER_ERROR,$translator->trans('front.error.not.template', ['template' => $template], domain: 'front_error') );
+        }
+
+        return 'front/' . $template;
+
+    }
+}
