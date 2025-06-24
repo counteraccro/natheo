8a06ad22d0886c83c56441c19cb62a34ffce0916
Aymeric <counteraccro@gmail.com>
2025-06-24 17:51:55 +0200

    Mise en place design

--- assets/vue/controllers/Front/Structure.vue
+++ assets/vue/controllers/Front/Structure.vue
@@ -27,5 +27,33 @@ export default {
 </script>
 
 <template>
-  <div class="markdown-editor-output" v-html="this.slug"></div>
+
+  <header>
+    Header
+  </header>
+  <nav class="h-10 rounded bg-gray-300">
+    Nav
+  </nav>
+  <main>
+    <div class="grid grid-cols-12 gap-2 mt-2">
+      <div class="col-span-12 md:col-span-2 md:order-1 rounded bg-gray-300">Menu</div>
+      <div class="col-span-12 md:col-span-8 md:order-2 order-3 rounded bg-gray-300">Content</div>
+      <div class="col-span-12 md:col-span-2 md:order-3 order-2 rounded bg-gray-300">Menu 2</div>
+    </div>
+
+    <div class="grid grid-cols-12 gap-2 mt-2">
+      <div class="col-span-12 md:col-span-2 rounded bg-gray-300">Menu</div>
+      <div class="col-span-12 md:col-span-10 rounded bg-gray-300">Content</div>
+    </div>
+
+    <div class="grid grid-cols-12 gap-2 mt-2">
+      <div class="col-span-12 md:col-span-10 md:order-1 order-2 rounded bg-gray-300">Content</div>
+      <div class="col-span-12 md:col-span-2 md:order-2 order-1 rounded bg-gray-300">Menu</div>
+    </div>
+
+  </main>
+
+  <footer class="h-10 rounded bg-gray-300 mt-2">
+    Footer
+  </footer>
 </template>
