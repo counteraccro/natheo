df669c26d7daa7b81ffabf1cead385d9c78fe5a5
Aymeric <counteraccro@gmail.com>
2025-08-07 07:38:11 +0200

    Avancement

--- assets/vue/Components/Front/NatheoHorizon/Nav/Nav.vue
+++ assets/vue/Components/Front/NatheoHorizon/Nav/Nav.vue
@@ -46,24 +46,26 @@ export default {
         <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
           <div class="flex-shrink-0 flex items-center">
             <img class="block h-16 w-auto" src="/assets/natheo/front/logo_transparent.png" alt="Logo">
-            <span class="ml-2 text-xl font-bold text-gray-800 dark:text-gray-200">{{ this.optionsSystem.OS_SITE_NAME }}</span>
+            <span class="ml-2 text-xl font-bold text-gray-800">{{
+                this.optionsSystem.OS_SITE_NAME
+              }}</span>
           </div>
           <div class="hidden lg:block sm:ml-6">
             <div class="flex space-x-4 items-center h-16">
 
 
-                <!-- menu type sidebar -->
-                <div v-for="element in this.data.elements" v-if="this.data.type === this.MenuType.headerSideBar">
-                  <a
-                      :href="this.utilsFront.getUrl(element)"
-                      class="!text-gray-500 hover:bg-theme-4-750 hover:!text-theme-1-100 px-3 py-2 rounded-md text-sm font-medium"
-                      :target="element.target">
-                    {{ element.label }}
-                  </a>
-                </div>
+              <!-- menu type sidebar -->
+              <div v-for="element in this.data.elements" v-if="this.data.type === this.MenuType.headerSideBar">
+                <a
+                    :href="this.utilsFront.getUrl(element)"
+                    class="!text-gray-500 hover:bg-theme-4-750 hover:!text-theme-1-100 px-3 py-2 rounded-md text-sm font-medium hover:dark:bg-gray-600"
+                    :target="element.target">
+                  {{ element.label }}
+                </a>
+              </div>
 
-                <!-- menu type dropdown -->
-                <div v-for="element in this.data.elements" v-if="this.data.type === this.MenuType.headerDropDown">
+              <!-- menu type dropdown -->
+              <div v-for="element in this.data.elements" v-if="this.data.type === this.MenuType.headerDropDown">
                 <a v-if="!element.hasOwnProperty('elements')" :href="this.utilsFront.getUrl(element)"
                    class="!text-gray-500 hover:bg-theme-4-750 hover:!text-theme-1-100 px-3 py-2 rounded-md text-sm font-medium"
                    :target="element.target">{{ element.label }}</a>
@@ -73,7 +75,7 @@ export default {
                 />
               </div>
 
-                <div v-for="element in this.data.elements" v-if="this.data.type === this.MenuType.headerBigMenu
+              <div v-for="element in this.data.elements" v-if="this.data.type === this.MenuType.headerBigMenu
               || this.data.type === this.MenuType.headerBigMenu2column || this.data.type === this.MenuType.headerBigMenu3column ||
               this.data.type === this.MenuType.headerBigMenu4column">
                 <a v-if="!element.hasOwnProperty('elements')" :href="this.utilsFront.getUrl(element)"
@@ -92,9 +94,9 @@ export default {
         <div class="absolute inset-y-0 right-0 flex items-center pr-2 lg:static lg:inset-auto lg:ml-6 lg:pr-0">
           <div class="hidden lg:flex lg:items-center">
 
-            <button onclick="(() => document.body.classList.toggle('dark'))()"
-                    class="h-10 w-10 rounded-lg p-2 hover:bg-gray-100 dark:hover:bg-gray-700 me-2">
-              <svg class="fill-violet-700 block dark:hidden" fill="currentColor" viewBox="0 0 20 20">
+            <button onclick="(() => document.documentElement.classList.toggle('dark'))()"
+                    class="group h-10 w-10 rounded-full p-2 hover:bg-theme-4-750 dark:hover:bg-gray-600 me-2 cursor-pointer">
+              <svg class="fill-violet-700 block group-hover:fill-theme-1-100 dark:hidden" fill="currentColor" viewBox="0 0 20 20">
                 <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
               </svg>
               <svg class="fill-yellow-500 hidden dark:block" fill="currentColor" viewBox="0 0 20 20">
@@ -111,9 +113,10 @@ export default {
 
             <button v-else class="flex items-center space-x-2 focus:outline-none">
               <div class="relative">
-                  <div class="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-                    <span class="font-medium text-gray-600 dark:text-gray-300">{{ this.userInfo.avatar }}</span>
-                  </div>
+                <div
+                    class="relative inline-flex items-center justify-center w-10 h-10 overflow-hidden bg-theme-4-750 rounded-full dark:bg-gray-600">
+                  <span class="font-medium text-theme-1-100 dark:text-gray-300">{{ this.userInfo.avatar }}</span>
+                </div>
               </div>
               <div class="hidden lg:flex flex-col items-start">
                 <span class="text-sm font-medium text-gray-500">{{ this.userInfo.login }}</span>
@@ -157,9 +160,29 @@ export default {
         </ul>
         <div class="pt-4 pb-3 border-t border-gray-200">
           <div class="flex items-center px-3 space-y-2 flex-col">
-            <a href="#"
+
+            <div class="group block w-full text-center hover:bg-theme-4-750 hover:!text-theme-1-100 rounded-md text-base cursor-pointer"
+                 onclick="(() => document.documentElement.classList.toggle('dark'))()">
+              <div
+                  class="h-10 w-10 rounded-lg p-2 m-auto">
+                <svg class="fill-violet-700 group-hover:fill-theme-1-100 block dark:hidden" fill="currentColor" viewBox="0 0 20 20">
+                  <path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path>
+                </svg>
+                <svg class="fill-yellow-500 hidden dark:block" fill="currentColor" viewBox="0 0 20 20">
+                  <path
+                      d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z"
+                      fill-rule="evenodd" clip-rule="evenodd"></path>
+                </svg>
+              </div>
+            </div>
+
+            <a :href="this.urls.adminAuth" v-if="this.userInfo === ''"
                class="block w-full text-center !text-gray-500 hover:bg-theme-4-750 hover:!text-theme-1-100 px-3 py-2 rounded-md text-base font-medium">
-              Login
+              {{ this.translate.login }}
+            </a>
+            <a :href="this.urls.logout" v-else
+               class="block w-full text-center !text-gray-500 hover:bg-theme-4-750 hover:!text-theme-1-100 px-3 py-2 rounded-md text-base font-medium">
+              [{{ this.userInfo.login }}] {{ this.translate.logout }}
             </a>
           </div>
         </div>
