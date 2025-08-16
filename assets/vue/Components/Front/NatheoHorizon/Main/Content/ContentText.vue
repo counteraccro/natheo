168254edd8c64f772c938e2ce9498ee92ea790c6
Aymeric <counteraccro@gmail.com>
2025-08-16 07:27:19 +0200

    Avancement

--- assets/vue/Components/Front/NatheoHorizon/Main/Content/ContentText.vue
+++ assets/vue/Components/Front/NatheoHorizon/Main/Content/ContentText.vue
@@ -20,6 +20,26 @@ export default {
   },
   mounted() {
 
+    /**
+     * Ajoute un span à la fin de chaque ligne
+     */
+    document.querySelectorAll("pre code").forEach(codeEl => {
+      if (codeEl.dataset.lnProcessed === "1") return;
+
+      if (codeEl.querySelector(".code-line")) {
+        codeEl.dataset.lnProcessed = "1";
+        return;
+      }
+
+      const html = codeEl.innerHTML;
+      let lines = html.split(/\r?\n/);
+      if (lines.length && lines[lines.length - 1].trim() === "") lines.pop();
+
+      const wrapped = lines
+          .map(line => `<span class="code-line">${line.length ? line : "&nbsp;"}</span>`).join("");
+      codeEl.innerHTML = wrapped;
+      codeEl.dataset.lnProcessed = "1";
+    });
   },
   computed: {
     output() {
