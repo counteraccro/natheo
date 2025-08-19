31f3446be05f055eafd2389b87f398d74aaf3f3d
Aymeric <counteraccro@gmail.com>
2025-08-19 08:02:32 +0200

    Mise en place FAQ

--- assets/vue/Components/Front/NatheoHorizon/Main/Content/ContentFaq.vue
+++ assets/vue/Components/Front/NatheoHorizon/Main/Content/ContentFaq.vue
@@ -17,6 +17,7 @@ export default {
   data() {
     return {
       isLoad: false,
+      faq: null,
     }
   },
   created() {
@@ -31,6 +32,7 @@ export default {
     loadContent() {
       let success = (datas) => {
         console.log(datas)
+        this.faq = datas
       }
 
       let loader = () => {
@@ -52,7 +54,26 @@ export default {
 
 <template>
   <div v-if="this.isLoad">
-    content faq
+
+    <div v-for="category in this.faq.content.categories" class="space-y-4">
+      <h2 class="text-slate-900 text-2xl font-semibold mb-3 mt-3">{{ category.title }}</h2>
+      <details v-for="question in category.questions" class="group [&_summary::-webkit-details-marker]:hidden">
+        <summary class="flex items-center justify-between gap-1.5 rounded-md border border-gray-200 bg-white shadow-sm p-4 text-gray-900 group-open:bg-theme-4-750 group-open:!text-theme-1-100 group-open:dark:bg-gray-600 hover:bg-theme-4-750 hover:!text-theme-1-100 hover:cursor-pointer hover:dark:bg-gray-600">
+          <h2 class="text-lg">{{ question.title }}</h2>
+          <svg
+              class="size-5 shrink-0 transition-transform duration-300 group-open:-rotate-180"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor">
+            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+          </svg>
+        </summary>
+        <p class="px-4 pt-4 text-slate-600">
+          {{ question.answer }}
+        </p>
+      </details>
+    </div>
   </div>
   <div v-else>
     <div class="space-y-6 p-4">
