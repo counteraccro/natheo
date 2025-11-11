e6244c3e2a3c78f2349fc3d3b459ab73a7a92d90
Aymeric <counteraccro@gmail.com>
2025-11-11 09:46:02 +0100

    Ajout icone

--- assets/vue/controllers/Admin/Content/TagForm.vue
+++ assets/vue/controllers/Admin/Content/TagForm.vue
@@ -174,7 +174,10 @@ export default {
 
         return icon + this.translate.btnSubmitCreate;
       }
-      return this.translate.btnSubmitUpdate;
+
+      let icon =
+        '<svg class="icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-width="2" d="M10.779 17.779 4.36 19.918 6.5 13.5m4.279 4.279 8.364-8.643a3.027 3.027 0 0 0-2.14-5.165 3.03 3.03 0 0 0-2.14.886L6.5 13.5m4.279 4.279L6.499 13.5m2.14 2.14 6.213-6.504M12.75 7.04 17 11.28"></path></svg>';
+      return icon + this.translate.btnSubmitUpdate;
     },
 
     /**
