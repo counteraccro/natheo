1b53e33226301058e2a511c6f40a6ceda805745c
Aymeric <counteraccro@gmail.com>
2025-11-19 23:29:06 +0100

    Ajout icon

--- assets/vue/controllers/Admin/GenericGrid.vue
+++ assets/vue/controllers/Admin/GenericGrid.vue
@@ -792,7 +792,18 @@ export default {
     @close-modal="this.hideModal"
     :option-show-close-btn="false"
   >
-    <template #title> <i class="bi bi-sign-stop"></i> {{ translate.confirmTitle }} </template>
+    <template #icon>
+      <svg class="h-6 w-6 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+        <path
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M12 13V8m0 8h.01M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+        />
+      </svg>
+    </template>
+    <template #title> {{ translate.confirmTitle }} </template>
     <template #body>
       <div v-html="msgConfirm"></div>
     </template>
