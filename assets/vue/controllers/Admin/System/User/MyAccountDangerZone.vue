a6536857e7da58819db836a578dc0075803f4288
Aymeric <counteraccro@gmail.com>
2025-11-20 23:09:26 +0100

    Avancement refonte

--- assets/vue/controllers/Admin/System/User/MyAccountDangerZone.vue
+++ assets/vue/controllers/Admin/System/User/MyAccountDangerZone.vue
@@ -43,7 +43,7 @@ export default {
     delete(confirm) {
       this.confirm_delete = confirm;
       if (!confirm) {
-        this.modalDelete.show();
+        this.showModal('delete');
       } else {
         axios
           .post(this.url_delete, {})
@@ -112,6 +112,10 @@ export default {
     hideDisabledModale() {
       this.hideModal('disabled');
     },
+
+    hideDeleteModale() {
+      this.hideModal('delete');
+    },
   },
 };
 </script>
@@ -147,7 +151,7 @@ export default {
           <span v-if="this.isReplace()">{{ this.translate.replace_description }}</span>
         </p>
       </div>
-      <button v-if="this.isDelete()" class="btn btn-danger btn-md whitespace-nowrap">
+      <button v-if="this.isDelete()" @click="this.delete(false)" class="btn btn-danger btn-md whitespace-nowrap">
         <svg class="icon-sm" fill="none" stroke="currentColor" viewBox="0 0 24 24">
           <path
             stroke-linecap="round"
@@ -158,7 +162,7 @@ export default {
         </svg>
         {{ this.translate.btn_delete }}
       </button>
-      <button v-if="this.isReplace()" class="btn btn-danger btn-md whitespace-nowrap">
+      <button v-if="this.isReplace()" @click="this.delete(false)" class="btn btn-danger btn-md whitespace-nowrap">
         <svg
           class="icon-sm"
           aria-hidden="true"
@@ -274,130 +278,98 @@ export default {
     </template>
   </modal>
 
-  <!-- old to delete -->
-
-  <!-- Modal disabled -->
-  <div
-    class="modal fade"
-    id="modal-alert-disabled"
-    tabindex="-1"
-    aria-labelledby="modal-alert"
-    aria-hidden="true"
-    data-bs-backdrop="static"
-    data-bs-keyboard="false"
+  <modal
+    :id="'modal-delete-user'"
+    :show="this.modal.delete"
+    @close-modal="this.hideDeleteModale"
+    :option-show-close-btn="false"
   >
-    <div class="modal-dialog modal-dialog-centered">
-      <div class="modal-content">
-        <div class="modal-header bg-danger">
-          <h1 class="modal-title fs-5 text-white">{{ this.translate.disabled_title }}</h1>
-          <button
-            v-if="!this.confirm_disabled"
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          ></button>
+    <template #icon>
+      <svg class="h-6 w-6 me-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+        <path
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M12 13V8m0 8h.01M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+        />
+      </svg>
+    </template>
+    <template #title>
+      <span v-if="this.isDelete()">{{ this.translate.delete_title }}</span>
+      <span v-if="this.isReplace()">{{ this.translate.replace_title }}</span>
+    </template>
+    <template #body>
+      <div v-if="!this.confirm_delete">
+        <div v-if="this.isDelete()">
+          <i class="bi bi-exclamation-triangle-fill"></i> {{ this.translate.delete_content_1 }} <br />
+          <p class="mt-2">
+            <i>{{ this.translate.delete_content_2 }}</i>
+          </p>
         </div>
-        <div class="modal-body">
-          <div v-if="!this.confirm_disabled">
-            <i class="bi bi-exclamation-triangle-fill"></i> {{ this.translate.disabled_content_1 }} <br />
-            <p class="mt-2">
-              <i>{{ this.translate.disabled_content_2 }}</i>
-            </p>
-          </div>
-          <div v-else>
-            <p v-html="this.msg_return"></p>
-          </div>
-        </div>
-        <div class="modal-footer">
-          <button v-if="!this.confirm_disabled" type="button" class="btn btn-secondary" data-bs-dismiss="modal">
-            {{ this.translate.disabled_btn_cancel }}
-          </button>
-          <button type="button" class="btn btn-danger" @click="this.disabled(true)">
-            {{ this.translate.disabled_btn_confirm }}
-          </button>
+        <div v-if="this.isReplace()">
+          <i class="bi bi-exclamation-triangle-fill"></i> {{ this.translate.replace_content_1 }} <br />
+          <p class="mt-2">
+            <i>{{ this.translate.replace_content_2 }}</i>
+          </p>
         </div>
       </div>
-    </div>
-  </div>
-
-  <!-- Modal delete -->
-  <div
-    class="modal fade"
-    id="modal-alert-delete"
-    tabindex="-1"
-    aria-labelledby="modal-alert"
-    aria-hidden="true"
-    data-bs-backdrop="static"
-    data-bs-keyboard="false"
-  >
-    <div class="modal-dialog modal-dialog-centered">
-      <div class="modal-content">
-        <div class="modal-header bg-danger">
-          <h1 v-if="this.isDelete()" class="modal-title fs-5 text-white">{{ this.translate.delete_title }}</h1>
-          <h1 v-if="this.isReplace()" class="modal-title fs-5 text-white">{{ this.translate.replace_title }}</h1>
-          <button
-            v-if="!this.confirm_delete"
-            type="button"
-            class="btn-close"
-            data-bs-dismiss="modal"
-            aria-label="Close"
-          ></button>
-        </div>
-        <div class="modal-body">
-          <div v-if="!this.confirm_delete">
-            <div v-if="this.isDelete()">
-              <i class="bi bi-exclamation-triangle-fill"></i> {{ this.translate.delete_content_1 }} <br />
-              <p class="mt-2">
-                <i>{{ this.translate.delete_content_2 }}</i>
-              </p>
-            </div>
-            <div v-if="this.isReplace()">
-              <i class="bi bi-exclamation-triangle-fill"></i> {{ this.translate.replace_content_1 }} <br />
-              <p class="mt-2">
-                <i>{{ this.translate.replace_content_2 }}</i>
-              </p>
-            </div>
-          </div>
-          <div v-else>
-            <p v-html="this.msg_return"></p>
-          </div>
-        </div>
-        <div class="modal-footer">
-          <button
-            v-if="!this.confirm_delete && this.isDelete()"
-            type="button"
-            class="btn btn-secondary"
-            data-bs-dismiss="modal"
-          >
-            {{ this.translate.delete_btn_cancel }}
-          </button>
-          <button
-            v-if="!this.confirm_delete && this.isReplace()"
-            type="button"
-            class="btn btn-secondary"
-            data-bs-dismiss="modal"
-          >
-            {{ this.translate.replace_btn_cancel }}
-          </button>
-          <button v-if="this.isDelete()" type="button" class="btn btn-danger" @click="this.delete(true)">
-            {{ this.translate.delete_btn_confirm }}
-          </button>
-          <button v-if="this.isReplace()" type="button" class="btn btn-danger" @click="this.delete(true)">
-            {{ this.translate.replace_btn_confirm }}
-          </button>
-        </div>
+      <div v-else>
+        <p v-html="this.msg_return"></p>
       </div>
-    </div>
-  </div>
+    </template>
+    <template #footer>
+      <button type="button" class="btn btn-danger btn-sm me-2" @click="this.delete(true)">
+        <svg
+          class="icon"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M8.5 11.5 11 14l4-4m6 2a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+          />
+        </svg>
 
-  <button class="btn btn-danger me-3" @click="this.disabled(false)">{{ this.translate.btn_disabled }}</button>
-  <button v-if="this.isDelete()" class="btn btn-danger" @click="this.delete(false)">
-    {{ this.translate.btn_delete }}
-  </button>
-  <button v-if="this.isReplace()" class="btn btn-danger" @click="this.delete(false)">
-    {{ this.translate.btn_replace }}
-  </button>
+        <span v-if="this.isDelete()"> {{ this.translate.delete_btn_confirm }}</span>
+        <span v-if="this.isReplace()"> {{ this.translate.replace_btn_confirm }}</span>
+      </button>
+
+      <button type="button" class="btn btn-outline-dark btn-sm" @click="this.hideModal('delete')">
+        <svg
+          class="icon"
+          aria-hidden="true"
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="m15 9-6 6m0-6 6 6m6-3a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+          />
+        </svg>
+
+        <span v-if="!this.confirm_delete && this.isDelete()">
+          {{ this.translate.delete_btn_cancel }}
+        </span>
+        <span v-if="!this.confirm_delete && this.isReplace()">
+          {{ this.translate.replace_btn_cancel }}
+        </span>
+      </button>
+    </template>
+  </modal>
 </template>
 
 <style scoped></style>
