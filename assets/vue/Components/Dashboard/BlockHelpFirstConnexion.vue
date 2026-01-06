7cbb83f5382a542f3489fe7c12b9455791331b01
Aymeric <counteraccro@gmail.com>
2026-01-06 10:59:45 +0100

    Avancement refonte

--- assets/vue/Components/Dashboard/BlockHelpFirstConnexion.vue
+++ assets/vue/Components/Dashboard/BlockHelpFirstConnexion.vue
@@ -158,7 +158,67 @@ export default {
       <AlertDanger v-if="this.errorMessage !== null" :text="this.errorMessage" />
 
       <AlertSuccess v-if="this.hideMsgSuccess" :text="this.translate.msg_hide_success" />
-      <div v-else>Content</div>
+      <div v-else>
+        <h4 class="font-semibold mb-2 text-[var(--text-primary)]">{{ this.translate.sub_title }}</h4>
+        <p class="text-sm mb-4 text-[var(--text-secondary)]">{{ this.translate.text_1 }}</p>
+
+        <div class="space-y-1">
+          <div v-for="(item, index) in this.result" :key="index">
+            <div v-if="item.success" class="config-item config-item-success">
+              <svg class="config-item-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+                ></path>
+              </svg>
+              <span> {{ item.msg }} </span>
+            </div>
+
+            <div v-else-if="!Array.isArray(item.msg)" class="config-item config-item-error">
+              <svg class="config-item-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                ></path>
+              </svg>
+              <span> {{ item.msg }} </span>
+            </div>
+            <div v-else>
+              <div class="config-item config-item-error">
+                <svg class="config-item-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  ></path>
+                </svg>
+                <span> {{ item.msgTitle }} </span>
+              </div>
+
+              <div
+                v-for="(subItem, index) in item.msg"
+                :key="index"
+                class="config-item config-item-error config-item-nested"
+              >
+                <svg class="config-item-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M10 14l2-2m0 0l2-2m-2 2l-2-2m2 2l2 2m7-2a9 9 0 11-18 0 9 9 0 0118 0z"
+                  ></path>
+                </svg>
+                <span>{{ subItem }} </span>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
     <div class="p-4" v-else>
       <SkeletonText />
