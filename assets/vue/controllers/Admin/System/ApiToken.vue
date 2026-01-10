7a9e51880fea7876f3671f3f3fa52cdee24f4755
Aymeric <counteraccro@gmail.com>
2026-01-10 15:52:29 +0100

    Avancement refonte

--- assets/vue/controllers/Admin/System/ApiToken.vue
+++ assets/vue/controllers/Admin/System/ApiToken.vue
@@ -236,6 +236,89 @@ export default {
         ></textarea>
         <div class="form-text">{{ this.translate.comment_help }}</div>
       </div>
+
+      <label for="token" class="form-label">{{ this.translate.token_label }}</label>
+      <div class="input-button-group">
+        <input
+          type="text"
+          class="form-input"
+          id="token"
+          :class="this.validation.token.isValide ? '' : 'is-invalid'"
+          v-model="this.apiToken.token"
+          disabled
+        />
+        <button class="btn btn-primary btn-sm" @click="this.generateToken()">
+          <svg class="icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z"
+            ></path>
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
+            ></path>
+          </svg>
+          {{ this.translate.btn_new_token }}
+        </button>
+        <button v-if="this.apiToken.id !== null" class="btn btn-dark btn-sm" type="button" @click="this.copyToken()">
+          <svg
+            class="icon"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M9 8v3a1 1 0 0 1-1 1H5m11 4h2a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1h-7a1 1 0 0 0-1 1v1m4 3v10a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1v-7.13a1 1 0 0 1 .24-.65L7.7 8.35A1 1 0 0 1 8.46 8H13a1 1 0 0 1 1 1Z"
+            />
+          </svg>
+
+          {{ this.translate.btn_copy_past }}
+        </button>
+      </div>
+      <div class="form-text text-error">{{ this.validation.token.msg }}</div>
+      <div class="form-text">
+        <span v-if="this.apiToken.id !== null">
+          {{ this.translate.input_token_help }}
+        </span>
+        <span v-else> {{ this.translate.input_token_help_add }} </span>
+      </div>
+
+      <div class="form-group mt-3">
+        <label for="roles" class="form-label">{{ this.translate.select_label_role }}</label>
+        <select class="form-input" id="roles" v-model="this.apiToken.roles[0]">
+          <option v-for="(role, key) in this.datas.roles" :value="key">{{ role }}</option>
+        </select>
+      </div>
+
+      <div class="alert alert-info-bordered">
+        <svg class="alert-icon" style="color: var(--alert-info)" fill="currentColor" viewBox="0 0 20 20">
+          <path
+            fill-rule="evenodd"
+            d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
+            clip-rule="evenodd"
+          ></path>
+        </svg>
+        <div class="alert-content">
+          <div class="alert-message">
+            <div>{{ this.translate.help_role }}</div>
+            <ul class="list-disc list-inside mt-1">
+              <li>{{ this.translate.help_role_read }}</li>
+              <li>{{ this.translate.help_role_write }}</li>
+              <li>{{ this.translate.help_role_admin }}</li>
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 
