abdb846bb441c8d75a9930e5d20ea66e0e223025
Aymeric <counteraccro@gmail.com>
2026-02-01 09:55:53 +0100

    Correction css

--- src/Twig/Extension/Admin/System/OptionExtension.php
+++ src/Twig/Extension/Admin/System/OptionExtension.php
@@ -127,7 +127,7 @@ class OptionExtension extends AppAdminExtension
     <ul class="flex flex-wrap -mb-px text-sm font-medium text-center" id="default-styled-tab" data-tabs-toggle="#nav-tab-option-system-content" data-tabs-active-classes="text-[var(--primary)] hover:text-[var(--primary-hover)] border-[var(--primary)] bg-[var(--primary-lighter)]" data-tabs-inactive-classes="dark:border-transparent text-gray-500 hover:text-gray-600 dark:text-gray-400 border-gray-100 hover:border-gray-300 dark:border-gray-700 dark:hover:text-gray-300" role="tablist">';
         foreach ($categories as $key => $category) {
             $html .=
-                '<li class="me-2" role="presentation"><button class="inline-block ps-4 pt-2 pe-4 pb-2 border-b-2 rounded-t-sm" id="nav-' .
+                '<li class="me-2" role="presentation"><button class="inline-block ps-4 pt-2 pe-4 pb-2 border-b-2 rounded-t-sm cursor-pointer" id="nav-' .
                 $key .
                 '-tab" data-tabs-target="#tab-' .
                 $key .
