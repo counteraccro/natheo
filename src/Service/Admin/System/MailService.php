c9c7ec9b63ad08ad0bbe74cfc5590c0930eddd64
Aymeric <counteraccro@gmail.com>
2026-02-13 06:03:52 +0100

    changement listing mail

--- src/Service/Admin/System/MailService.php
+++ src/Service/Admin/System/MailService.php
@@ -131,21 +131,22 @@ class MailService extends AppAdminService
      * @throws ContainerExceptionInterface
      * @throws NotFoundExceptionInterface
      */
-    public function getAllPaginate(int $page, int $limit): Paginator
+    public function getAllPaginate(int $page, int $limit, array $queryParams): Paginator
     {
         $repo = $this->getRepository(Mail::class);
-        return $repo->getAllPaginate($page, $limit);
+        return $repo->getAllPaginate($page, $limit, $queryParams);
     }
 
     /**
      * Construit le tableau de donnée à envoyer au tableau GRID
      * @param int $page
      * @param int $limit
+     * @param array $queryParams
      * @return array
      * @throws ContainerExceptionInterface
      * @throws NotFoundExceptionInterface
      */
-    public function getAllFormatToGrid(int $page, int $limit): array
+    public function getAllFormatToGrid(int $page, int $limit, array $queryParams): array
     {
         $translator = $this->getTranslator();
         $gridService = $this->getGridService();
@@ -159,7 +160,7 @@ class MailService extends AppAdminService
             GridService::KEY_ACTION,
         ];
 
-        $dataPaginate = $this->getAllPaginate($page, $limit);
+        $dataPaginate = $this->getAllPaginate($page, $limit, $queryParams);
 
         $nb = $dataPaginate->count();
         $data = [];
@@ -186,6 +187,13 @@ class MailService extends AppAdminService
             GridService::KEY_DATA => $data,
             GridService::KEY_COLUMN => $column,
             GridService::KEY_RAW_SQL => $gridService->getFormatedSQLQuery($dataPaginate),
+            GridService::KEY_LIST_ORDER_FIELD => [
+                'id' => $translator->trans('mail.grid.id', domain: 'mail'),
+                'title' => $translator->trans('mail.grid.title', domain: 'mail'),
+                'description' => $translator->trans('mail.grid.description', domain: 'mail'),
+                'createdAt' => $translator->trans('mail.grid.created_at', domain: 'mail'),
+                'updateAt' => $translator->trans('mail.grid.update_at', domain: 'mail'),
+            ],
         ];
         return $gridService->addAllDataRequiredGrid($tabReturn);
     }
@@ -205,7 +213,10 @@ class MailService extends AppAdminService
 
         // Bouton test email
         $actions[] = [
-            'label' => '<i class="bi bi-send-check-fill"></i>',
+            'label' => [
+                'm3.5 5.5 7.893 6.036a1 1 0 0 0 1.214 0L20.5 5.5M4 19h16a1 1 0 0 0 1-1V6a1 1 0 0 0-1-1H4a1 1 0 0 0-1 1v12a1 1 0 0 0 1 1Z',
+            ],
+            'color' => 'success',
             'type' => 'get',
             'url' => $router->generate('admin_mail_send_demo_mail', ['id' => $mail->getId()]),
             'ajax' => true,
@@ -214,7 +225,10 @@ class MailService extends AppAdminService
 
         // Bouton edit
         $actions[] = [
-            'label' => '<i class="bi bi-pencil-fill"></i>',
+            'label' => [
+                'M10.779 17.779 4.36 19.918 6.5 13.5m4.279 4.279 8.364-8.643a3.027 3.027 0 0 0-2.14-5.165 3.03 3.03 0 0 0-2.14.886L6.5 13.5m4.279 4.279L6.499 13.5m2.14 2.14 6.213-6.504M12.75 7.04 17 11.28',
+            ],
+            'color' => 'primary',
             'type' => 'get',
             'id' => $mail->getId(),
             'url' => $router->generate('admin_mail_edit', ['id' => $mail->getId()]),
