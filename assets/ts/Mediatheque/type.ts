b0d980242debeb62caeaabf0b602bdc28c4a0857
Aymeric <counteraccro@gmail.com>
2026-03-14 11:02:17 +0100

    Ajout type pour les objets médiatheque

---
+++ assets/ts/Mediatheque/type.ts
@@ -0,0 +1,30 @@
+export type TranslateRecord = { [key: string]: string | TranslateRecord };
+
+export type MediaFolder = {
+  type: 'folder';
+  id: number;
+  name: string;
+  created_at: number;
+  date: string;
+  size: string;
+  nb_element: number;
+};
+
+export type MediaFile = {
+  type: 'media';
+  id: number;
+  name: string;
+  description: string;
+  size: string;
+  webPath: string;
+  thumbnail: string;
+  created_at: number;
+  date: string;
+  extension: string;
+  img_size: string;
+  folder: string;
+};
+
+export type MediaItem = MediaFolder | MediaFile;
+
+export type MediaList = MediaItem[];
