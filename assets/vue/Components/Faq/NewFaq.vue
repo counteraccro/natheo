fd772c83f4bc147d66d1210bce42f70aebc9b852
BroGuillaume <guilllaume.brocart@gmail.com>
2026-06-08 16:04:24 +0200

    add 1 icon in http://dev.natheo:8888/admin/fr/faq/add/

--- assets/vue/Components/Faq/NewFaq.vue
+++ assets/vue/Components/Faq/NewFaq.vue
@@ -125,7 +125,27 @@ export default defineComponent({
   <div v-else>
     <div class="card rounded-lg p-6 mb-4 mt-4">
       <div class="border-b-1 border-b-[var(--border-color)] mb-4">
-        <h2 class="text-lg font-bold text-[var(--text-primary)]">{{ translate.new_faq }}</h2>
+        <h2 class="flex gap-2 text-lg font-bold text-[var(--text-primary)]">
+          <svg
+            class="icon-lg"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            fill="none"
+            viewBox="0 0 24 24"
+            style="color: var(--primary)"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M9.529 9.988a2.502 2.502 0 1 1 5 .191A2.441 2.441 0 0 1 12 12.582V14m-.01 3.008H12M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
+            />
+          </svg>
+          {{ translate.new_faq }}
+        </h2>
         <p class="text-sm mt-1 mb-3 text-[var(--text-secondary)]">{{ translate.new_faq_sub_title }}</p>
       </div>
 
