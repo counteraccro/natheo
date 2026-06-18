60fb8714c95a86276d436bd7734ebf01d1974733
Aymeric <counteraccro@gmail.com>
2026-06-18 20:12:22 +0200

    Création Enum OptionSystem

---
+++ src/Enum/Admin/System/Options/OptionSystem.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * Liste des clés pour les options Systèmes
+ * @author Gourdon Aymeric
+ * @version 1.0
+ */
+
+namespace App\Enum\Admin\System\Options;
+
+enum OptionSystem: string
+{
+    /**
+     * Clé option nom du site
+     */
+    case OS_SITE_NAME = 'OS_SITE_NAME';
+
+    const CONFIG = [
+        self::OS_SITE_NAME->value => ['default' => 'Nathéo CMS'],
+    ];
+
+    /**
+     * Récupère la valeur par défaut
+     * @return string|null
+     */
+    public function getDefault(): ?string
+    {
+        if (isset(self::CONFIG[$this->value]['default'])) {
+            return self::CONFIG[$this->value]['default'];
+        }
+        return null;
+    }
+}
