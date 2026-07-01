140f58a5e575e3a6d479d0fae3aac34b708e4144
BroGuillaume <guilllaume.brocart@gmail.com>
2026-07-01 14:56:10 +0200

    migrate OS_LOG_DOCTRINE from OptionSystemKey to OptionSystem

--- src/Utils/System/Options/OptionSystemKey.php
+++ src/Utils/System/Options/OptionSystemKey.php
@@ -11,12 +11,6 @@ namespace App\Utils\System\Options;
 class OptionSystemKey
 {
     /**
-     * Clé option qui permet de sauvegarder ou non les logs
-     * @var string
-     */
-    const string OS_LOG_DOCTRINE = 'OS_LOG_DOCTRINE';
-
-    /**
      * Clé option pour récupérer l'expéditeur des emails
      * @var string
      */
