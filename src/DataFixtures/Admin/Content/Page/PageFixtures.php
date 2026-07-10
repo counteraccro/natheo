30c63d565ed66b2134275572d46ca0c1bfeacaf8
Aymeric <counteraccro@gmail.com>
2026-07-10 08:03:38 +0200

    Aide pour @BroGuillaume

--- src/DataFixtures/Admin/Content/Page/PageFixtures.php
+++ src/DataFixtures/Admin/Content/Page/PageFixtures.php
@@ -35,6 +35,10 @@ class PageFixtures extends AppFixtures implements FixtureGroupInterface, Ordered
     {
         $data = Yaml::parseFile($this->pathDataFixtures . self::PAGE_FIXTURES_DATA_FILE);
 
+        //TODO clear var/pageHistory et pageHistory-test
+        // Voir FileSystem de Symfony. Attention au path, utiliser les paramètres internes
+        // Mettre le code dans src/Utils/Content/Page/PageHistory.php
+
         if ($data === null) {
             return;
         }
