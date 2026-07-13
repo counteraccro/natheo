b5588f5fad9640af6a4191d6effbdae18af5fe9a
Aymeric <counteraccro@gmail.com>
2026-07-13 07:10:24 +0200

    Mise en place gestion des tags

---
+++ assets/vue/Components/Page/PageTag.vue
@@ -0,0 +1,40 @@
+<script lang="ts">
+/**
+ * Gestionnaire des Page - onglet Tag
+ * @author Gourdon Aymeric
+ * @version 2.0
+ */
+import { defineComponent, PropType } from 'vue';
+import { PageTranslations } from '@/ts/Page/type';
+
+export default defineComponent({
+  name: 'PageTag',
+  props: {
+    translate: {
+      type: Object as PropType<PageTranslations>,
+      required: true,
+    },
+  },
+});
+</script>
+
+<template>
+  <div class="card mb-4">
+    <div class="card-header">
+      <div class="card-title">
+        <svg class="card-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+          <path
+            stroke="currentColor"
+            stroke-linecap="round"
+            stroke-width="2"
+            d="M7 7h.01M7 3h5c.512 0 1.024.195 1.414.586l7 7a2 2 0 010 2.828l-7 7a2 2 0 01-2.828 0l-7-7A1.994 1.994 0 013 12V7a4 4 0 014-4z"
+          />
+        </svg>
+        {{ translate.tag_title }}
+      </div>
+    </div>
+    <div class="p-5"></div>
+  </div>
+</template>
+
+<style scoped></style>
