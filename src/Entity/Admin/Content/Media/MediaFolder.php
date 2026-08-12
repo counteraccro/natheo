5470356103730ce9bc25faff43a0cf8fe17dbc20
Aymeric <counteraccro@gmail.com>
2026-08-12 07:58:22 +0200

    Passage enum Strategy au lieu de constante

--- src/Entity/Admin/Content/Media/MediaFolder.php
+++ src/Entity/Admin/Content/Media/MediaFolder.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 namespace App\Entity\Admin\Content\Media;
 
+use App\Enum\Installation\DoctrineStrategy;
 use App\Repository\Admin\Content\Media\MediaFolderRepository;
-use App\Utils\Installation\InstallationConst;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\DBAL\Types\Types;
@@ -17,7 +17,7 @@ use Doctrine\ORM\Mapping as ORM;
 class MediaFolder
 {
     #[ORM\Id]
-    #[ORM\GeneratedValue(strategy: InstallationConst::STRATEGY)]
+    #[ORM\GeneratedValue(strategy: DoctrineStrategy::CURRENT)]
     #[ORM\Column]
     private ?int $id = null;
 
