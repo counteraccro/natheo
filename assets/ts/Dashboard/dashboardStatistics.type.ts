a47e955f734ae4c3483a0ca805901f1a8b04651f
Aymeric <counteraccro@gmail.com>
2026-08-26 08:02:39 +0200

    Ajout type

---
+++ assets/ts/Dashboard/dashboardStatistics.type.ts
@@ -0,0 +1,11 @@
+export interface DashboardStatisticsTranslate {
+  total_article: string;
+  comments: string;
+  comments_waiting: string;
+  users: string;
+  views: string;
+}
+
+export interface Urls {
+  stats: string;
+}
